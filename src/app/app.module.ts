61ac97613dd949bdde6b27bd7b9ad2c8bc09c546
Salsabeel <salsabeel@tridz.com>
2017-07-10 16:37:35 +0530

    conf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+// components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+// Routes
+import { routes } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
