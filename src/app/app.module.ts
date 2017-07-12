78894c28a8db3f145961bb4a2d48aecc66c8375a
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-07-12 19:21:57 +0530

    initializing user events, jobs, job-details, notification, opportunity, portfolio, search and spotfeed-premium components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,14 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileBioComponent } from './components/profile-bio/profile-bio.component';
 import { MessageComponent } from './components/message/message.component';
 import { SpotfeedComponent } from './components/spotfeed/spotfeed.component';
+import { SpotfeedPremiumComponent } from './components/spotfeed-premium/spotfeed-premium.component';
+import { NotificationComponent } from './components/notification/notification.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { OpportunityComponent } from './components/opportunity/opportunity.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
+import { EventsComponent } from './components/events/events.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 
@@ -48,7 +56,15 @@ import { SpotfeedComponent } from './components/spotfeed/spotfeed.component';
     ProfileComponent,
     ProfileBioComponent,
     MessageComponent,
-    SpotfeedComponent
+    SpotfeedComponent,
+    SpotfeedPremiumComponent,
+    NotificationComponent,
+    PortfolioComponent,
+    OpportunityComponent,
+    JobsComponent,
+    JobDetailsComponent,
+    EventsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,6 +11,14 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileBioComponent } from './components/profile-bio/profile-bio.component';
 import { MessageComponent } from './components/message/message.component';
 import { SpotfeedComponent } from './components/spotfeed/spotfeed.component';
+import { SpotfeedPremiumComponent } from './components/spotfeed-premium/spotfeed-premium.component';
+import { NotificationComponent } from './components/notification/notification.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { OpportunityComponent } from './components/opportunity/opportunity.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
+import { EventsComponent } from './components/events/events.component';
+import { SearchComponent } from './components/search/search.component';
 
 //Guard
 import { AuthGuard } from './guard/auth.guard';
@@ -25,6 +33,14 @@ export const routes: Routes =[
  { path: 'profile-bio', component: ProfileBioComponent },
  { path: 'message', component: MessageComponent },
  { path: 'spotfeed', component: SpotfeedComponent },
+ { path: 'spotfeed-premium', component: SpotfeedPremiumComponent },
+ { path: 'notification', component: NotificationComponent },
+ { path: 'portfolio', component: PortfolioComponent },
+ { path: 'opportunity', component: OpportunityComponent },
+ { path: 'jobs', component: JobsComponent },
+ { path: 'job-details', component: JobDetailsComponent },
+ { path: 'events', component: EventsComponent },
+ { path: 'search', component: SearchComponent },
  // otherwise redirect to home
   { path: '**', redirectTo: '' }
 ];
