2597090da5054c13e29e12f12f820c913749f857
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-07-17 19:31:04 +0530

    registration http request with rxjs is done

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -18,6 +18,8 @@ import { JobDetailsComponent } from './components/job-details/job-details.compon
 import { EventsComponent } from './components/events/events.component';
 import { SearchComponent } from './components/search/search.component';
 import { SettingsComponent } from './components/settings/settings.component';
+import { RegistrationProfileComponent } from './components/registration-profile/registration-profile.component';
+import { RegistrationAddSkillComponent } from './components/registration-add-skill/registration-add-skill.component';
 import { RegistrationBasicComponent } from './components/registration-basic/registration-basic.component';
 import { RegistrationWelcomeComponent } from './components/registration-welcome/registration-welcome.component';
 
@@ -41,6 +43,8 @@ export const routes: Routes = [
  { path: 'events', component: EventsComponent },
  { path: 'search', component: SearchComponent },
  { path: 'settings', component: SettingsComponent },
+ { path: 'registration/profile', component: RegistrationProfileComponent },
+ { path: 'registration/addskill', component: RegistrationAddSkillComponent },
  { path: 'registration-basic', component: RegistrationBasicComponent },
  { path: 'registration-welcome', component: RegistrationWelcomeComponent },
  // otherwise redirect to home
