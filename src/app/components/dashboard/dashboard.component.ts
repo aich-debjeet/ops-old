dcbc01ac849525eac01edd4b921cba2f765311fe
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-07-31 12:27:31 +0530

    loading initial channel list to display

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  constructor() { }
+  cards: any = [];
 
-  ngOnInit() {
+  constructor() {
+
+    // initial cards
+    this.cards = [{
+      title: 'title 1',
+      image: 'http://via.placeholder.com/640x360',
+      descirption: '1 Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+      links: [
+        { linkRight: 'http://testlink-right.com' },
+        { linkLeft: 'http://testlink-left.com' }
+      ]
+    }, {
+      title: 'title 2',
+      image: 'http://via.placeholder.com/640x360',
+      descirption: '2 Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+      links: [
+        { linkRight: 'http://testlink-right.com' },
+        { linkLeft: 'http://testlink-left.com' }
+      ]
+    }, {
+      title: 'title 3',
+      image: 'http://via.placeholder.com/640x360',
+      descirption: '3 Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+      links: [
+        { linkRight: 'http://testlink-right.com' },
+        { linkLeft: 'http://testlink-left.com' }
+      ]
+    }, {
+      title: 'title 4',
+      image: 'http://via.placeholder.com/640x360',
+      descirption: '4 Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+      links: [
+        { linkRight: 'http://testlink-right.com' },
+        { linkLeft: 'http://testlink-left.com' }
+      ]
+    }, {
+      title: 'title 5',
+      image: 'http://via.placeholder.com/640x360',
+      descirption: '5 Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+      links: [
+        { linkRight: 'http://testlink-right.com' },
+        { linkLeft: 'http://testlink-left.com' }
+      ]
+    }];
+
   }
 
 }
