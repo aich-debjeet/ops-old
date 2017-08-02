ddcdee48c549f6630c2c8c2afb5083146534b1d8
Salsabeel <k.salsabeel@gmail.com>
2017-08-02 20:37:52 +0530

    opportunity

--- src/app/components/opportunity/opportunity.component.ts
+++ src/app/components/opportunity/opportunity.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-opportunity',
   templateUrl: './opportunity.component.html',
   styleUrls: ['./opportunity.component.css']
 })
-export class OpportunityComponent implements OnInit {
+export class OpportunityComponent {
 
-  constructor() { }
+  jobs: any = [];
 
-  ngOnInit() {
+  constructor() {
+
+    // initial cards
+    this.jobs = [{
+      position: 'creative director',
+      experience: '2-4',
+      location: 'mumbai'
+    }, {
+      position: 'designer',
+      experience: '0-1',
+      location: 'bangalore'
+    }, {
+      position: 'jr web developer',
+      experience: '0-1',
+      location: 'delhi'
+    }, {
+      position: 'full stack developer',
+      experience: '2-4',
+      location: 'kolkata'
+    }, {
+      position: 'sr tech manager',
+      experience: '5-10',
+      location: 'banglaore'
+    }, {
+      position: 'test',
+      experience: 'test',
+      location: 'test'
+    }];
+
   }
 
 }
