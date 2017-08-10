7b63511d32cc6f19df334a7485a767522ee74a47
Debjeet Aich <debjeet.aich@aeione.com>
2017-08-10 11:51:43 +0530

    message component created

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer } from '@angular/core';
 
 @Component({
   selector: 'app-message',
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  navOpen: boolean = false;
+  composeMessageOpen: boolean = false;
+  messageDisplayOpen: boolean = false;
+
+  constructor(private render: Renderer) { }
 
   ngOnInit() {
+  }
+
+  composeDisplayToggle() {
+    if(this.composeMessageOpen) {
+      this.composeMessageOpen = false;
+    } else {
+      this.composeMessageOpen = true;
+      //this.messageDisplayOpen = false;
+      console.log("its true");
+      //this.messageDisplayOpen = true;
+    }
+  }
+  messageDisplayToggle() {
+    if(this.messageDisplayOpen) {
+       this.messageDisplayOpen = false;
+   } else {
+     this.composeMessageOpen = false;
+     this.messageDisplayOpen = true;
+     console.log("its true");
+   }
+ }
+
+  toggleNav(event:any) {
+
+    event.preventDefault();
+    if(this.navOpen) {
+      this.navOpen = false;
+    } else {
+      this.navOpen = true;
+    }
+
   }
 
 }
