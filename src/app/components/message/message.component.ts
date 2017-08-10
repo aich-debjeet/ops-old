8628b133ef4183e96fb5aa0656178dbc38feefbb
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-08-10 17:55:05 +0530

    added missing icons and markup rearranged

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -7,44 +7,7 @@ import { Component, OnInit, Renderer } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-  navOpen: boolean = false;
-  composeMessageOpen: boolean = false;
-  messageDisplayOpen: boolean = false;
-
-  constructor(private render: Renderer) { }
-
   ngOnInit() {
-  }
-
-  composeDisplayToggle() {
-    if(this.composeMessageOpen) {
-      this.composeMessageOpen = false;
-    } else {
-      this.composeMessageOpen = true;
-      //this.messageDisplayOpen = false;
-      console.log("its true");
-      //this.messageDisplayOpen = true;
-    }
-  }
-  messageDisplayToggle() {
-    if(this.messageDisplayOpen) {
-       this.messageDisplayOpen = false;
-   } else {
-     this.composeMessageOpen = false;
-     this.messageDisplayOpen = true;
-     console.log("its true");
-   }
- }
-
-  toggleNav(event:any) {
-
-    event.preventDefault();
-    if(this.navOpen) {
-      this.navOpen = false;
-    } else {
-      this.navOpen = true;
-    }
-
   }
 
 }
