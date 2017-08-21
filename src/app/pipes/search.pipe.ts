a1e369b5c1010667c819032216627fe582888700
Debjeet Aich <debjeet.aich@aeione.com>
2017-08-21 17:50:10 +0530

    searchIcon component created

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -6,13 +6,15 @@ import { Injectable, Pipe, PipeTransform } from '@angular/core';
 
 @Injectable()
 export class SearchFilterPipe implements PipeTransform {
- transform(items: any[], field: string): any[] {
+ transform(items: any[], field: string, value: string): any[] {
      if (!field){
+       console.log(items);
         return items;
       } else {
+        console.log('field'+field);
         return items
-          .filter(item => 
-            item.name.toLowerCase().indexOf(field.toString().toLowerCase()) !== -1 
+          .filter(item =>
+          item.name.toLowerCase().indexOf(field.toString().toLowerCase()) !== -1
           );
       }
  }
