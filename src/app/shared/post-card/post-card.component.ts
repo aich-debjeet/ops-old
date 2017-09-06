57117211e1c30c2c60940b2a620fd80a8ccecd3e
Muneef Hameed <hey@muneef.in>
2017-09-06 18:09:24 +0530

    update: profile, card styles

--- src/app/shared/post-card/post-card.component.ts
+++ src/app/shared/post-card/post-card.component.ts
@@ -13,12 +13,23 @@ export class PostCardComponent implements OnInit {
   @Input() mediaData;
   @Input() type: string;
   @Output() onClick: EventEmitter<any> = new EventEmitter<any>();
+  dotMenuState: boolean;
 
   private imageLink: string = environment.API_IMAGE;
 
-  constructor() { }
+  constructor() {
+    this.dotMenuState = false;
+  }
 
   ngOnInit() {
+  }
+
+  /**
+   * Delete Media from Channel
+   * @param event
+   */
+  deleteMedia(channel: any) {
+    console.log('Deleting this Channenl');
   }
 
   handleClick(event: any) {
@@ -27,5 +38,11 @@ export class PostCardComponent implements OnInit {
 
   checkFileType(fileName: string, fileType: string) {
     return FilesHelper.fileType(fileName, fileType);
+  }
+
+  dotMenuOpen() {
+    console.log('Opening');
+    this.dotMenuState = !this.dotMenuState;
+    console.log(this.dotMenuState);
   }
 }
