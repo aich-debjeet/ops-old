43c28c6e75cff9560038a06946538fa457a1f1e1
Muneef Hameed <hey@muneef.in>
2017-09-22 14:54:14 +0530

    deploy: changed to AOT

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-ng build --env=prod --output-path=build/prod/
+ng build --prod --aot --output-path=build/prod/
 scp -i ~/dev/DevMongoCluster.pem -r build/prod/* ec2-user@54.183.144.207:/usr/share/nginx/html
