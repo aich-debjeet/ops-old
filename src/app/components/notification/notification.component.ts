3a25603dc254b1c713e1c6d9e6bb148857371a44
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-10-10 19:03:17 +0530

    Cleaning up notifications source code

--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -6,57 +6,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./notification.component.scss']
 })
 export class NotificationComponent implements OnInit {
-  dropDownOpen : boolean = false;
-  messageIconOpen : boolean = false;
-  notificationOpen : boolean = false;
-  searchBarDisplay : boolean = false;
+
   constructor() { }
 
-  ngOnInit() {
-  }
-    toggleNav(event:any) {
-    event.preventDefault();
-    if(this.dropDownOpen) {
-      this.dropDownOpen = false;
-    } else {
-      this.dropDownOpen = true;
-      this.messageIconOpen = false;
-      this.notificationOpen = false;
-      this.searchBarDisplay = false;
-    }
-  }
-  toggleNav1(event:any) {
-    event.preventDefault();
-    if(this.messageIconOpen) {
-      this.messageIconOpen = false;
-    } else {
-      this.messageIconOpen = true;
-      this.dropDownOpen = false;
-      this.notificationOpen = false;
-      this.searchBarDisplay = false;
-    }
-  }
-  toggleNav2(event:any) {
-    event.preventDefault();
-    if(this.notificationOpen) {
-      this.notificationOpen = false;
-    } else {
-      this.notificationOpen = true;
-      this.searchBarDisplay = false;
-      this.dropDownOpen = false;
-      this.messageIconOpen = false;
-    }
-  }
-  searchBar()
-  {
-    if (this.searchBarDisplay) {
-      this.searchBarDisplay=false;
-    }
-    else {
-      this.searchBarDisplay=true;
-      this.notificationOpen=false;
-      this.dropDownOpen=false;
-      this.messageIconOpen=false;
-    }
-  }
+  ngOnInit() { }
+
 }
