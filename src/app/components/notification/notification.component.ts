27dff210e575225ab6ad6fe16473465efc21d24a
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-10-11 14:33:41 +0530

    Working on notifications

--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -8,6 +8,7 @@ import { Subscription } from 'rxjs/Subscription';
 // action
 import { NotificationActions } from './../../actions/notification.action';
 import { Notification } from './../../models/notification.model';
+import { environment } from './../../../environments/environment';
 
 @Component({
   selector: 'app-notification',
@@ -17,10 +18,22 @@ import { Notification } from './../../models/notification.model';
 export class NotificationComponent implements OnInit {
 
   notificationsState$: Observable<Notification>;
+  formattedNotifications: any[];
+  notifications: any[];
+  baseUrl: string;
+  msgTypes = [
+    'Media_Spot',
+    'Media_Comments',
+    'Status_Spot',
+    'Status_Comments'
+  ];
 
   constructor(
     private store: Store<Notification>
   ) {
+
+    // image path
+    this.baseUrl = environment.API_IMAGE;
 
     // loading notifications
     this.store.dispatch({
@@ -32,8 +45,55 @@ export class NotificationComponent implements OnInit {
 
     // observe the store value
     this.notificationsState$.subscribe((state) => {
-      console.log(state);
+      // console.log(state);
+      if (typeof state['completed'] !== 'undefined') {
+        this.notifications = state['completed'];
+        this.processNotifications();
+      }
+      console.log('this.notifications', this.notifications);
     });
+  }
+
+  // message maker
+  processNotifications() {
+
+    this.notifications.forEach((notif, index) => {
+      console.log();
+
+      switch (notif.notificationType) {
+
+        case 'Media_Spot':
+          this.notifications[index]['message'] = notif.name + ' and ' + notif.spotCount + ' others spotted your post';
+          break;
+
+        case 'Media_Comments':
+          this.notifications[index]['message'] = notif.name + ' and ' + notif.commentsCount + ' others commented on your post';
+          break;
+
+        case 'Status_Spot':
+          this.notifications[index]['message'] = notif.name + ' and ' + notif.spotCount + ' others spotted your status';
+          break;
+
+        case 'Status_Comments':
+          this.notifications[index]['message'] = notif.name + ' and ' + notif.commentsCount + ' others commented on your status';
+          break;
+
+      }
+
+      if (index === (this.notifications.length - 1)) {
+        console.log('last', this.notifications[index]);
+        // return this.notifications;
+      }
+    });
+
+  }
+
+  /**
+   * Marking notification as read
+   * @Param: notification id
+   */
+  markAsRead(notificationId: string) {
+    console.log(notificationId);
   }
 
   ngOnInit() { }
