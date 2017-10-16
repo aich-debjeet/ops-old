113a22caab41a9a0c50190c8a5bb2d1849ec63ca
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-10-16 14:26:19 +0530

    Working on search page

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel, RoutesRecognized } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  constructor() {}
+  previousUrl: string;
+  constructor(router: Router) {
+    router.events
+    .filter(event => event instanceof NavigationEnd)
+    .subscribe(e => {
+      this.previousUrl = e['url'];
+      // console.log('prev:', this.previousUrl);
+    });
+  }
 
   ngOnInit() {
   }
