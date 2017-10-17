b356b4054afbf23a8e2f82f36bb9dacf751bd32e
Salsabeel <k.salsabeel@gmail.com>
2017-10-18 00:02:46 +0530

    Hotfix search redirect

--- src/app/components/search/search.routes.ts
+++ src/app/components/search/search.routes.ts
@@ -10,14 +10,16 @@ import { SearchComponent } from './search.component'
 import { SearchCommunityComponent } from './search-community/search-community.component';
 
 export const SearchRoutes = [
-  { path: '', component: SearchComponent ,
-  children: [
-    { path: 'all', component: SearchAllComponent },
-    { path: 'channel', component: SearchChannelComponent },
-    { path: 'community', component: SearchCommunityComponent },
-    { path: 'people', component: SearchPeopleComponent },
-    { path: 'post', component: SearchPostComponent },
-    { path: 'spotfeed', component: SearchSpotfeedComponent }
-  ]
+ {
+    path: 'search',
+    component: SearchComponent,
+    children: [
+      { path: 'all', component: SearchAllComponent },
+      { path: 'channel', component: SearchChannelComponent },
+      { path: 'community', component: SearchCommunityComponent },
+      { path: 'people', component: SearchPeopleComponent },
+      { path: 'post', component: SearchPostComponent },
+      { path: 'spotfeed', component: SearchSpotfeedComponent }
+    ]
  }
 ]
