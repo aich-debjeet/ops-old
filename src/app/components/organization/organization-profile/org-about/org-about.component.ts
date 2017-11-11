9678a86ea7dd512f4e2a3137b505ab66f12f38de
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-11-11 16:51:08 +0530

    Add: Edit bio industry dropdown added

--- src/app/components/organization/organization-profile/org-about/org-about.component.ts
+++ src/app/components/organization/organization-profile/org-about/org-about.component.ts
@@ -4,12 +4,15 @@ import { Store } from '@ngrx/store';
 // action
 import { ProfileActions } from '../../../../actions/profile.action';
 import { OrganizationActions } from '../../../../actions/organization.action';
+import { AuthActions } from '../../../../actions/auth.action';
 
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
 import { Organization, initialOrganization } from '../../../../models/organization.model';
 import { UtcDatePipe } from './../../../../pipes/utcdate.pipe';
+
+import { initialTag, Follow } from '../../../../models/auth.model';
 
 @Component({
   selector: 'app-org-about',
@@ -21,6 +24,9 @@ export class OrgAboutComponent implements OnInit {
   orgState$: Observable<Organization>;
   orgProfile;
   editingField: string;
+  orgIndustry: string;
+  forIndustries: any;
+  loginTagState$: Observable<Follow>;
 
   constructor(
     private store: Store<Organization>
@@ -30,9 +36,23 @@ export class OrgAboutComponent implements OnInit {
     this.orgState$ = this.store.select('organizationTags');
     this.orgState$.subscribe((state) => {
       this.orgProfile = state;
-      console.log('this.orgProfile ABOUT ORG', this.orgProfile);
+      // console.log('this.orgProfile ABOUT ORG', this.orgProfile);
+      if (this.orgProfile && this.orgProfile.orgProfileDetails && this.orgProfile.orgProfileDetails['industryList'].length > 0) {
+        setTimeout(() => {
+          const industryArrLen = this.orgProfile.orgProfileDetails['industryList'].length;
+          this.orgIndustry = this.orgProfile.orgProfileDetails['industryList'][industryArrLen - 1].code;
+          // console.log('this.orgIndustry', this.orgIndustry);
+        }, 1000);
+      }
     });
     /* org state */
+
+    this.loginTagState$ = store.select('loginTags');
+    this.loginTagState$.subscribe((state) => {
+      this.forIndustries = state;
+    });
+
+    this.store.dispatch({ type: AuthActions.LOAD_INDUSTRIES});
 
   }
 
