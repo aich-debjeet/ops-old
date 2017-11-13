bbb6e14a40c5204cac5e2ea5dc69469c1426be72
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-11-13 17:01:00 +0530

    About insdustry update done

--- src/app/components/organization/organization-profile/org-about/org-about.component.ts
+++ src/app/components/organization/organization-profile/org-about/org-about.component.ts
@@ -13,6 +13,7 @@ import { Organization, initialOrganization } from '../../../../models/organizati
 import { UtcDatePipe } from './../../../../pipes/utcdate.pipe';
 
 import { initialTag, Follow } from '../../../../models/auth.model';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-org-about',
@@ -24,28 +25,42 @@ export class OrgAboutComponent implements OnInit {
   orgState$: Observable<Organization>;
   orgProfile;
   editingField: string;
-  orgIndustry: string;
+  aboutIndustry: any;
+  aboutIndustryCode: any;
   forIndustries: any;
   loginTagState$: Observable<Follow>;
   aboutMobile: any;
+  aboutDescription: string;
+  aboutServices: string;
+  // services: any[];
 
   constructor(
     private store: Store<Organization>
   ) {
 
+    // this.services = ['UI design', 'Web Application Development', 'Social Media'];
+
     /* org state */
     this.orgState$ = this.store.select('organizationTags');
     this.orgState$.subscribe((state) => {
       this.orgProfile = state;
-      // console.log('this.orgProfile ABOUT ORG', this.orgProfile);
+      console.log('this.orgProfile ABOUT ORG', this.orgProfile);
       if (this.orgProfile && this.orgProfile.org_profile_details && this.orgProfile.org_profile_details.contact.mobile) {
         this.aboutMobile = this.orgProfile.org_profile_details.contact.mobile;
+      }
+      if (this.orgProfile && this.orgProfile.org_profile_details && this.orgProfile.org_profile_details.description) {
+        this.aboutDescription = this.orgProfile.org_profile_details.description;
+      }
+      if (this.orgProfile && this.orgProfile.org_profile_details && this.orgProfile.org_profile_details.languages) {
+        this.aboutServices = this.orgProfile.org_profile_details.languages.join(', ');
+        // console.log('aboutServices', this.aboutServices);
       }
       if (this.orgProfile && this.orgProfile.org_profile_details && this.orgProfile.org_profile_details.extra['industryList'].length > 0) {
         setTimeout(() => {
           const industryArrLen = this.orgProfile.org_profile_details.extra['industryList'].length;
-          this.orgIndustry = this.orgProfile.org_profile_details.extra['industryList'][industryArrLen - 1].code;
-          console.log('this.orgIndustry', this.orgIndustry);
+          this.aboutIndustry = this.orgProfile.org_profile_details.extra['industryList'][industryArrLen - 1];
+          this.aboutIndustryCode = this.aboutIndustry['code'];
+          // console.log('this.aboutIndustry', this.aboutIndustry);
         }, 1000);
       }
     });
@@ -54,7 +69,7 @@ export class OrgAboutComponent implements OnInit {
     this.loginTagState$ = store.select('loginTags');
     this.loginTagState$.subscribe((state) => {
       this.forIndustries = state;
-      console.log('this.forIndustries', this.forIndustries);
+      // console.log('this.forIndustries', this.forIndustries);
     });
 
     this.store.dispatch({ type: AuthActions.LOAD_INDUSTRIES});
@@ -79,28 +94,71 @@ export class OrgAboutComponent implements OnInit {
   }
 
   /**
+   * Closing editors
+   */
+  closeEditor() {
+    this.cancelEdit();
+  }
+
+  /**
    * Update about individual field
    */
   updateAbout(fieldName: string) {
-    // console.log('update org field', fieldName);
+    console.log('update org field', fieldName);
 
     let reqBody;
 
-    if (fieldName === 'mobile') {
+    if (fieldName === 'mobile' && this.aboutMobile.length > 0) {
       reqBody.mobile = this.aboutMobile;
-      // console.log('reqBody.mobile', reqBody.mobile);
     }
-    
-    // if () {
-      
-    // }
 
-    const data = {
-      handle: this.orgProfile.orgProfileDetails.handle,
-      body: reqBody
+    if (fieldName === 'description' && this.aboutDescription.length > 0) {
+      reqBody = {
+        extras: {
+          Description: ''
+        }
+      };
+      reqBody.extras.Description = this.aboutDescription;
     }
-    // console.log('data', data);
+
+    if (fieldName === 'services' && this.aboutServices.length > 0) {
+      reqBody = {
+        services: [],
+      };
+      const aboutServicesArr = this.aboutServices.split(',');
+      aboutServicesArr.forEach((service, index) => {
+        reqBody.services.push(service);
+        // console.log('index', index);
+        if (index >= (aboutServicesArr.length - 1)) {
+          this.dispatchUpdate(reqBody);
+          return;
+        }
+      });
+    }
+
+    if (fieldName === 'industries' && this.aboutIndustryCode.length > 0) {
+      console.log('selected', this.aboutIndustryCode);
+      const newIndustry = _.find(this.forIndustries.industries, { 'code': this.aboutIndustryCode });
+      // console.log('newIndustry', newIndustry);
+      // console.log('this.forIndustries.industries', this.forIndustries.industries);
+      reqBody = {
+        industryList: []
+      };
+      reqBody.industryList.push(newIndustry);
+    }
+
+    this.dispatchUpdate(reqBody);
+  }
+
+  dispatchUpdate(reqData: any) {
+    const data = {
+      handle: this.orgProfile.org_profile_details.handle,
+      body: reqData
+    }
+    console.log('req body', data);
     this.store.dispatch({ type: OrganizationActions.ORG_PROFILE_UPDATE, payload: data });
+
+    this.closeEditor();
   }
 
 }
