bb7d543565844bcddae6ddb9bf9225210730c3a9
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-11-17 22:35:05 +0530

    Working on opportunity structure

---
+++ src/app/components/opportunity/opportunity-search/opportunity-search-recommended/opportunity-search-recommended.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opportunity-search-recommended',
+  templateUrl: './opportunity-search-recommended.component.html',
+  styleUrls: ['./opportunity-search-recommended.component.scss']
+})
+export class OpportunitySearchRecommendedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/opportunity/opportunity.module.ts
+++ src/app/components/opportunity/opportunity.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { OpportunitySearchComponent } from './opportunity-search/opportunity-search.component';
-import { OpportunityCreateComponent } from './opportunity-create/opportunity-create.component';
+
 import { OpportunityComponent } from './opportunity.component';
+
+import { OpportunityCreateComponent } from './opportunity-create/opportunity-create.component';
+
+import { OpportunitySearchComponent } from './opportunity-search/opportunity-search.component';
+import { OpportunitySearchRecommendedComponent } from './opportunity-search/opportunity-search-recommended/opportunity-search-recommended.component';
+import { OpportunitySearchAppliedComponent } from './opportunity-search/opportunity-search-applied/opportunity-search-applied.component';
+import { OpportunitySearchCreatedComponent } from './opportunity-search/opportunity-search-created/opportunity-search-created.component';
 
 // opportunity module routes
 const opportunityRoutes: Routes = [
   {
     path: '',
     component: OpportunityComponent,
-    // redirectTo: 'search',
     children: [
-      { path: 'search', component: OpportunitySearchComponent },
+      {
+        path: 'search',
+        component: OpportunitySearchComponent,
+        children: [
+          { path: 'recommended', component: OpportunitySearchRecommendedComponent },
+          { path: 'applied', component: OpportunitySearchAppliedComponent },
+          { path: 'created', component: OpportunitySearchCreatedComponent }
+        ]
+      },
       { path: 'create', component: OpportunityCreateComponent }
     ]
   }
@@ -24,9 +37,12 @@ const opportunityRoutes: Routes = [
     RouterModule.forChild(opportunityRoutes)
   ],
   declarations: [
-    OpportunitySearchComponent,
+    OpportunityComponent,
     OpportunityCreateComponent,
-    OpportunityComponent
+    OpportunitySearchComponent,
+    OpportunitySearchRecommendedComponent,
+    OpportunitySearchAppliedComponent,
+    OpportunitySearchCreatedComponent
   ]
 })
 export class OpportunityModule { }
