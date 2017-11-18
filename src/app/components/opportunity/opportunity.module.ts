3f673882fa60ca13c2d1ca85c7fc28be2e0b2f45
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-11-18 15:25:49 +0530

    Wroking on create opportunities

--- src/app/components/opportunity/opportunity.module.ts
+++ src/app/components/opportunity/opportunity.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { OpportunityComponent } from './opportunity.component';
-
 import { OpportunityCreateComponent } from './opportunity-create/opportunity-create.component';
-
 import { OpportunitySearchComponent } from './opportunity-search/opportunity-search.component';
 import { OpportunitySearchRecommendedComponent } from './opportunity-search/opportunity-search-recommended/opportunity-search-recommended.component';
 import { OpportunitySearchAppliedComponent } from './opportunity-search/opportunity-search-applied/opportunity-search-applied.component';
 import { OpportunitySearchCreatedComponent } from './opportunity-search/opportunity-search-created/opportunity-search-created.component';
+
+// shared module
+import { SharedModule } from '../../shared/shared.module';
+import { MasonryModule } from 'angular2-masonry';
 
 // opportunity module routes
 const opportunityRoutes: Routes = [
@@ -34,6 +36,8 @@ const opportunityRoutes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
+    MasonryModule,
     RouterModule.forChild(opportunityRoutes)
   ],
   declarations: [
@@ -43,6 +47,9 @@ const opportunityRoutes: Routes = [
     OpportunitySearchRecommendedComponent,
     OpportunitySearchAppliedComponent,
     OpportunitySearchCreatedComponent
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class OpportunityModule { }
