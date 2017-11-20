0dfcde5de581e44c193e5cff3c06d30b05b845fe
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-11-20 17:47:44 +0530

    Working on job application

--- src/app/components/opportunity/opportunity-view/opportunity-view.component.ts
+++ src/app/components/opportunity/opportunity-view/opportunity-view.component.ts
@@ -25,6 +25,10 @@ export class OpportunityViewComponent implements OnInit {
   opportunityState$: any;
   opportunityState: any;
   opportunity: any;
+  jobId: any;
+  userProfile: any;
+  userState$: any;
+  hasApplied: boolean;
 
   baseUrl = environment.API_IMAGE;
 
@@ -32,15 +36,35 @@ export class OpportunityViewComponent implements OnInit {
     private route: ActivatedRoute,
     private store: Store<OpportunityModel>
   ) {
-    // state listener
+    // opportunity state listener
     this.opportunityState$ = this.store.select('opportunityTags');
     this.opportunityState$.subscribe((state) => {
       this.opportunityState = state;
-      console.log('view opp comp state', state);
+      // console.log('view opp comp state', state);
+
+      // get opp data
       if (state && state.get_opportunity_data && state.get_opportunity_data.SUCCESS) {
         this.opportunity = state.get_opportunity_data.SUCCESS;
+        this.hasApplied = state.get_opportunity_data.SUCCESS.isAppliable;
+        console.log('isAppliable', this.hasApplied);
+      }
+
+      // check if job application successful
+      if (state && state.apply_for_an_opportunity_data  && state.apply_for_an_opportunity_data.SUCCESS) {
+        this.hasApplied = true;
       }
     });
+
+    /**
+     * check user state
+     */
+    // this.userState$ = this.store.select('profileTags');
+    // this.userState$.subscribe((state) => {
+    //   if (state && state.profileUser) {
+    //     this.userProfile = state.profileUser;
+    //     console.log('this.userProfile', this.userProfile);
+    //   }
+    // });
   }
 
   ngOnInit() {
@@ -48,9 +72,23 @@ export class OpportunityViewComponent implements OnInit {
       console.log(params['id']);
       if (params['id']) {
         // search job with id
-        const jobId = params['id'];
-        this.store.dispatch({ type: OpportunityActions.GET_OPPORTUNITY, payload: jobId });
+        this.jobId = params['id'];
+        this.store.dispatch({ type: OpportunityActions.GET_OPPORTUNITY, payload: this.jobId });
       }
+    });
+  }
+
+  /**
+   * apply for job
+   */
+  applyForJob() {
+    const reqBody = {
+      jobId: this.jobId
+    }
+
+    this.store.dispatch({
+      type: OpportunityActions.APPLY_FOR_AN_OPPORTUNITY,
+      payload: reqBody
     });
   }
 
