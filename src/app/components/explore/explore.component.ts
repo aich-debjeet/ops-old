a27e3f4c0a446e9fb9e5f01e728da026f83253c7
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-12-06 14:25:15 +0530

    Add: Static page integration

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -6,17 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./explore.component.scss']
 })
 export class ExploreComponent implements OnInit {
-  comingsoon: any;
   constructor() {}
 
   ngOnInit() {
-    this.comingsoon = {
-      mainTitle: 'Explore <br> Coming Soon.',
-      description: 'Our Explorers are working up the map. Feel free to browse through our other pages.',
-      buttonLink: '/home',
-      buttonText: 'Go to Home',
-      img: 'https://d33wubrfki0l68.cloudfront.net/aa7151c6bd31e22c19fee9e6753360fe77abcb99/7b1df/img/svg/404/explore_coming_soon_02.png'
-    };
   }
 
 }
