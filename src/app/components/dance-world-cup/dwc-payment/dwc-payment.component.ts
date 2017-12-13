06b0cbc5bfe7067f56112c1c3692e2dd8cc7ea9d
Salsabeel <k.salsabeel@gmail.com>
2017-12-14 00:25:23 +0530

    DWC Detail Page update

---
+++ src/app/components/dance-world-cup/dwc-payment/dwc-payment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dwc-payment',
+  templateUrl: './dwc-payment.component.html',
+  styleUrls: ['./dwc-payment.component.scss']
+})
+export class DwcPaymentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/dance-world-cup/dwc-progress-bar/dwc-progress-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dwc-progress-bar',
+  templateUrl: './dwc-progress-bar.component.html',
+  styleUrls: ['./dwc-progress-bar.component.scss']
+})
+export class DwcProgressBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
