87cce31d12f89ce911a467f235da4e771c31b642
Debjeet Aich <debjeet.aich@aeione.com>
2017-12-13 22:05:06 +0530

    dwc form update

--- src/app/components/dwc/dwc.component.ts
+++ src/app/components/dwc/dwc.component.ts
@@ -5,6 +5,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Modal } from '../../shared/modal-new/Modal';
 
+
 // Action
 import { AuthActions } from '../../actions/auth.action'
 
@@ -15,6 +16,9 @@ import { AuthActions } from '../../actions/auth.action'
 })
 export class DwcComponent implements OnInit {
   public eventForm: FormGroup;
+  Performance = ['Solo', 'Couple/Trio', 'Group'];
+  Age_Group = ['Mini - 9 years and under', 'Junior section - 17 years and under', 'Children – 13 years and under', 'Senior section - 25 years and under']
+  Dance_Style = ['Classical Ballet', 'National, Folklore, Character', 'Modern, Contemporary', 'Jazz and show dance', 'Hip Hop, Street Dance and Commercial', 'Song and Dance', 'Tap', 'Fusion Ballet'];
   @ViewChild('dwcModal') DwctypeModal: Modal;
   constructor(
     private fb: FormBuilder,
@@ -27,6 +31,7 @@ export class DwcComponent implements OnInit {
   ngOnInit() {
     this.buildForm();
     this.loadScript('https://js.instamojo.com/v1/button.js');
+    console.log(this.Performance);
   }
 
   testClick() {
@@ -44,21 +49,38 @@ export class DwcComponent implements OnInit {
    * Form init
    */
   buildForm() {
+    const defaultPerformance = [];
+    const defaultAge_Group = [];
+    const defaultDance_Style = [];
     this.eventForm = this.fb.group({
+      Performance   : this.fb.array(this.Performance.map(x => defaultPerformance.indexOf(x) > -1)),
+      Age_Group : this.fb.array(this.Age_Group.map(x => defaultAge_Group.indexOf(x) > -1)),
+      Dance_Style: this.fb.array(this.Dance_Style.map(x => defaultDance_Style.indexOf(x) > -1)),
       'school_name' : ['', [Validators.required]],
+      'member' : ['', [Validators.required]],
       'school_owner' : ['', [Validators.required]],
       'school_address' : ['', [Validators.required]],
-      'school_category' : ['', [Validators.required]],
-      'school_teammates' : ['', [Validators.required]],
-      'otherInformation' : ['Solo', [Validators.required]],
-      'category_dance' : ['', [Validators.required]],
-      'age_group' : ['', [Validators.required]]
-    })
+      'teammates_Name': ['', [Validators.required]],
+      'teammates_Email': ['', [Validators.required]],
+      'teammates_Phone': ['', [Validators.required]],
+      // 'event_agenda' : this.fb.array([]),
+    });
 
   }
 
+  convertToValue(key: string) {
+    console.log(key)
+    console.log(this.eventForm.value)
+    return this.eventForm.value[key].map((x, i) => x && this[key][i]).filter(x => !!x);
+  }
 // dwc_event_reg_success
   submitForm(value) {
+    const valueToStore = Object.assign({}, this.eventForm.value, {
+      Performance: this.convertToValue('Performance'),
+      Age_Group: this.convertToValue('Age_Group'),
+      Dance_Style: this.convertToValue('Dance_Style'),
+    });
+    console.log(valueToStore);
     if (this.eventForm.valid) {
       // const form = {
       //   schoolName: value.school_name,
@@ -81,12 +103,22 @@ export class DwcComponent implements OnInit {
         schoolName: value.school_name,
         schoolOwner: value.school_owner,
         address: value.school_address,
-        category: value.school_category,
-        teamates: value.school_teammates,
-        otherInformation: value.otherInformation,
-        ageGroup: value.age_group,
-        danceCategory: value.category_dance,
+        // category: value.school_category,
+        // teamates: value.school_teammates,
+        // otherInformation: value.otherInformation,
+        // ageGroup: value.age_group,
+        // danceCategory: value.category_dance,
+        performance: value.Performance,
+        ageGroup: value.Age_Group,
+        danceStyle: value.Dance_Style,
+        member: value.member,
+        teamates: {
+          name: value.teammates_Name,
+          email: value.teammates_Name,
+          phone: value.teammates_Name
+        },
       }
+      console.log(form)
     this.router.navigateByUrl('/profile/user');
     this.store.dispatch({ type: AuthActions.DWC_EVENT_REG, payload: form});
   }
