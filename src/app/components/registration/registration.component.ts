2a79654596b9dd78888f1675ba38452b9d422ef8
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2017-12-13 23:32:08 +0530

    Signup minor cleaup

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -9,7 +9,7 @@ export class RegValue {
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.scss']
 })
 
 export class RegistrationComponent implements OnInit {
