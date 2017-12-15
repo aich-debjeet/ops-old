67e654f148967c2227852f103d3d2baadccd0fc0
Salsabeel <k.salsabeel@gmail.com>
2017-12-15 18:15:23 +0530

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: 'https://p1.api.onepagespotlight.com/api/1.0',
+  API_ENDPOINT: 'https://p.api.onepagespotlight.com/api/1.0',
   // API_ENDPOINT: 'https://devservices.greenroom6.com:9000/api/1.0',
   API_IMAGE: 'https://d206s58i653k1q.cloudfront.net/'
 };
