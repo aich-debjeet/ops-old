6f11375e9525194935a0f9df808cd1c6ff4cc041
Salsabeel <k.salsabeel@gmail.com>
2017-12-20 12:17:40 +0530

    Replace http

--- src/app/components/home/home-right-block/home-right-block.component.ts
+++ src/app/components/home/home-right-block/home-right-block.component.ts
@@ -11,6 +11,7 @@ import { Store } from '@ngrx/store';
 import { ProfileModal, initialTag } from '../../../models/profile.model';
 import { filter as _filter } from 'lodash';
 import { OpportunityActions } from '../../../actions/opportunity.action';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'home-right-block',
@@ -24,6 +25,7 @@ export class HomeRightBlockComponent implements OnInit {
   tagState$: Observable<ProfileModal>;
   private tagStateSubscription: Subscription;
   myProfile$: Observable<any>;
+  baseUrl = environment.API_IMAGE;
   userState: any;
   profiles = [];
   skillCodes = [];
