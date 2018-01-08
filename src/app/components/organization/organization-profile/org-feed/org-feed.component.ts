c222a66d10a0206fed89859b3d9792c9f960acd8
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-01-08 15:45:33 +0530

    Working on org spotfeeds

--- src/app/components/organization/organization-profile/org-feed/org-feed.component.ts
+++ src/app/components/organization/organization-profile/org-feed/org-feed.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { DatePipe } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { UserSpotfeeds } from '../../../../models/user-spotfeed.model';
+
+// action
+import { ProfileActions } from '../../../../actions/profile.action';
+
+// rx
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-org-feed',
@@ -7,7 +18,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrgFeedComponent implements OnInit {
 
-  constructor() { }
+  tagState$: Observable<UserSpotfeeds>;
+  private tagStateSubscription: Subscription;
+  userState;
+  spotfeeds: any = [];
+  baseUrl: String;
+
+  constructor(
+    private http: Http,
+    private store: Store<UserSpotfeeds>
+  ) {
+
+    this.tagState$ = this.store.select('profileTags');
+    this.tagState$.subscribe((state) => {
+      this.userState = state;
+      if (this.userState.home_spotfeeds !== undefined && this.userState.home_spotfeeds.SUCCESS !== undefined) {
+        this.spotfeeds = this.userState.home_spotfeeds.SUCCESS;
+      }
+    });
+    this.store.dispatch({ type: ProfileActions.LOAD_HOME_PAGE_SPOTFEEDS });
+  }
 
   ngOnInit() {
   }
