3e447ba796de5db932c6b68c534938bb0aa277ca
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-01-18 11:18:18 +0530

    Reading video dimentions

--- src/app/shared/videplayer/videplayer.component.ts
+++ src/app/shared/videplayer/videplayer.component.ts
@@ -1,19 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-videplayer',
   templateUrl: './videplayer.component.html',
   styleUrls: ['./videplayer.component.scss']
 })
-export class VideplayerComponent implements OnInit {
+export class VideplayerComponent implements OnInit, AfterViewInit {
   @Input() src: any;
   @Input() play: boolean = false;
   source: any;
+  isPortrait = false; // assuming video is landscape by default
   constructor() {
     this.source = this.src;
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    const videoElem: HTMLVideoElement = <HTMLVideoElement>document.getElementById('singleVideo');
+    console.log('videoElem', videoElem);
+    const self = this;
+    videoElem.addEventListener('loadeddata', function() {
+      console.log('videoElem loaded');
+      const vidH = videoElem.videoHeight;
+      const vidW = videoElem.videoWidth;
+      console.log(vidH, vidW);
+
+      // check if video is portrait
+      if (vidH > vidW) {
+        self.isPortrait = true;
+        console.log('video portrait');
+      } else {
+        console.log('video lanscape');
+      }
+    });
   }
 
 }
