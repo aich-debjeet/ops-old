f2479b2e5fe968c265e5ebceffdfd9918c2c6179
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-02-15 13:03:43 +0530

    Update: Authentication added to the org images, Req body

--- src/app/components/organization/organization-profile/org-cover/org-cover.component.ts
+++ src/app/components/organization/organization-profile/org-cover/org-cover.component.ts
@@ -96,10 +96,10 @@ export class OrgCoverComponent implements OnInit {
   }
 
   fileChangeListener($event) {
-      let image: any = new Image();
-      let file: File = $event.target.files[0];
-      let myReader: FileReader = new FileReader();
-      let that = this;
+      const image: any = new Image();
+      const file: File = $event.target.files[0];
+      const myReader: FileReader = new FileReader();
+      const that = this;
       myReader.onloadend = function (loadEvent: any) {
           image.src = loadEvent.target.result;
           that.cropper.setImage(image);
@@ -116,6 +116,7 @@ export class OrgCoverComponent implements OnInit {
     const userHandle = this.orgHandle;
     if (this.data && this.data.image && userHandle !== '') {
       const imageData = {
+        imageType: 'coverImage',
         handle: userHandle,
         image: this.data.image.split((/,(.+)/)[1])
       };
