14b1b65f93db61a9290a0ebb3d646ea6c9e01d02
Salsabeel <k.salsabeel@gmail.com>
2018-04-03 11:54:03 +0530

    map field to Reative Form

--- src/app/components/communities/communities.component.ts
+++ src/app/components/communities/communities.component.ts
@@ -2,6 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl, FormArray } from '@angular/forms';
 
+// Action
+import { AuthActions } from '../../actions/auth.action';
+
+// rx
+import { Observable } from 'rxjs/Observable';
+import { Subscription, ISubscription } from 'rxjs/Subscription';
+import { Store } from '@ngrx/store';
+
+
 @Component({
   selector: 'app-communities',
   templateUrl: './communities.component.html',
@@ -9,23 +18,34 @@ import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl, FormA
 })
 export class CommunitiesComponent implements OnInit {
 
-  comingsoon: any;
+  industries: any[];
   basePath = environment.API_IMAGE;
   public communityForm: FormGroup;
+  industryState$: Observable<any>;
+  private subscription: ISubscription;
+  selectedIndustry = '';
   constructor(
     private fb: FormBuilder,
+    private store: Store<any>
   ) {
+    this.industryState$ = store.select('loginTags');
+    this.subscription = this.industryState$.subscribe((state) => {
+      if (typeof state !== 'undefined') {
+        this.industries = state.industries;
+      }
+    });
   }
 
   ngOnInit() {
     this.buildForm();
+    this.store.dispatch({ type: AuthActions.LOAD_INDUSTRIES });
   }
 
   buildForm() {
     this.communityForm = this.fb.group({
       'community_name' : ['', [Validators.required]],
       'brief': ['', [Validators.required]],
-      'access': ['', [Validators.required]],
+      'access': [0, [Validators.required]],
       'industry': ['', [Validators.required]]
     })
 
@@ -33,5 +53,13 @@ export class CommunitiesComponent implements OnInit {
 
   submitForm(value) {
     console.log(value);
+    const data = {
+      title: value.community_name,
+      brief: value.brief,
+      accessSettings: {
+        access: 0
+      },
+      industryList: []
+    }
   }
 }
