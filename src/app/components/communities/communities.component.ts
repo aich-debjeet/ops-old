0b13b36601494f68656c1d31879648b5f095205b
Salsabeel <k.salsabeel@gmail.com>
2018-04-03 11:23:20 +0530

    community form update

--- src/app/components/communities/communities.component.ts
+++ src/app/components/communities/communities.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-communities',
@@ -10,9 +11,27 @@ export class CommunitiesComponent implements OnInit {
 
   comingsoon: any;
   basePath = environment.API_IMAGE;
-  constructor() {
+  public communityForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+  ) {
   }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.communityForm = this.fb.group({
+      'community_name' : ['', [Validators.required]],
+      'brief': ['', [Validators.required]],
+      'access': ['', [Validators.required]],
+      'industry': ['', [Validators.required]]
+    })
+
+  }
+
+  submitForm(value) {
+    console.log(value);
   }
 }
