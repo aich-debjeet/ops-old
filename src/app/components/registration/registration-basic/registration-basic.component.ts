b95f46376b75eae1c24602c28ef775dd7661121b
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-05-14 19:45:00 +0530

    Reset: Signup basic

--- src/app/components/registration/registration-basic/registration-basic.component.ts
+++ src/app/components/registration/registration-basic/registration-basic.component.ts
@@ -35,7 +35,6 @@ export class RegValue {
   loginLink: Boolean;
   button_text: string;
   button_link: string;
-  username: FormControl;
 }
 
 @Component({
@@ -57,9 +56,8 @@ export class RegistrationBasicComponent implements OnInit, OnDestroy {
   hideProfiles = false;
   inputNameListener: any;
   showTerms = false;
-  username = new FormControl();
-  usernameCtrlSub: Subscription;
 
+  rightCom: RightBlockTag;
   tagState$: Observable<BasicRegTag>;
   private tagStateSubscription: Subscription;
   petTag = initialBasicRegTag;
@@ -97,26 +95,15 @@ export class RegistrationBasicComponent implements OnInit, OnDestroy {
     public tokenService: TokenService
     ) {
 
-    // username exists check
-    this.usernameCtrlSub = this.username.valueChanges
-      .debounceTime(1000)
-      .subscribe((value) => {
-        if (value.length >= 4) {
-          this.store.dispatch({ type: AuthActions.USER_EXISTS_CHECK, payload: value });
-        } else {
-          if (this.petTag && this.petTag.user_unique) {
-            this.petTag.user_unique = false;
-          }
-        }
-      });
-
     this.tagState$ = store.select('loginTags');
     this.tagState$.subscribe((state) => {
+
       if (typeof state !== 'undefined') {
+        // console.log(state);
         this.petTag = state;
+        // console.log(this.petTag['user_exist']);
       }
     });
-
     this.isPhotoAdded = false;
 
     // if redriect url there
@@ -134,6 +121,7 @@ export class RegistrationBasicComponent implements OnInit, OnDestroy {
 
   // terms show/hide
   termsAction(action: string) {
+    // console.log('terms value', this.showTerms);
     if (action === 'hide') {
       this.showTerms = false;
     } else {
@@ -157,9 +145,7 @@ export class RegistrationBasicComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    // setTimeout(() => {
-    //   this.modalService.open('otpWindow');
-    // }, 5000);
+    // document.body.style.overflow = 'hidden';
 
     if (this.route.snapshot.queryParams['ev']) {
       if (this.route.snapshot.queryParams['ev'] === 'dwc2017') {
@@ -173,6 +159,18 @@ export class RegistrationBasicComponent implements OnInit, OnDestroy {
 
     const currentUrl = this.router.url;
     // console.log(currentUrl);
+
+    this.rightCom = {
+      mainTitle: 'Create Your Account',
+      secondHead: '',
+      description: 'Welcome to One Page Spotlight family where we are committed to grow together.'
+        + ' An OTP number will be sent to your email or phone number after registration for account verification.',
+      loginLink: true,
+      button_text: 'Login',
+      button_link: '/login',
+      page: false,
+      img: 'https://d33wubrfki0l68.cloudfront.net/2e71b712243279d510245bad8c3e48eeab00690d/7f58a/img/registration_signup_illustration.png'
+    };
   }
 
   // Init Reg Form
@@ -192,17 +190,17 @@ export class RegistrationBasicComponent implements OnInit, OnDestroy {
         this.databaseValidator.validAge.bind(this.databaseValidator)
       ],
       'email' : ['', [
-          Validators.required,
-          Validators.min(1),
-          // Validators.email
-          FormValidation.validEmail
+        Validators.required,
+        Validators.min(1),
+        // Validators.email
+        FormValidation.validEmail
         ],
         this.databaseValidator.checkEmail.bind(this.databaseValidator)
       ],
       'gender': ['M', Validators.required],
       'phone' : ['', [
-          Validators.required,
-          Validators.minLength(4)
+        Validators.required,
+        Validators.minLength(4)
         ],
         this.databaseValidator.checkMobile.bind(this.databaseValidator)
       ],
@@ -234,6 +232,17 @@ export class RegistrationBasicComponent implements OnInit, OnDestroy {
         this.databaseValidator.checkMobile.bind(this.databaseValidator)
       ]
     })
+  }
+
+  // User user exists
+  userExistCheck(value) {
+    if (value.length >= 4) {
+      this.store.dispatch({ type: AuthActions.USER_EXISTS_CHECK, payload: value });
+    } else {
+      if (this.petTag && this.petTag.user_unique) {
+        this.petTag.user_unique = false;
+      }
+    }
   }
 
   // OTP Validation
@@ -269,6 +278,16 @@ export class RegistrationBasicComponent implements OnInit, OnDestroy {
     } else {
       number = this.regFormBasic.value.phone;
     }
+
+    // const form =  {
+    //   'client_id' : 'AKIAI7P3SOTCRBKNR3IA',
+    //   'client_secret': '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',
+    //   'username' : number.toString(),
+    //   'password' : this.otpForm.value.otpNumber,
+    //   'grant_type' : 'password'
+    // }
+    // this.store.dispatch({ type: AuthActions.OTP_LOGIN_SUBMIT, payload: form });
+
   }
 
   /**
@@ -395,9 +414,24 @@ export class RegistrationBasicComponent implements OnInit, OnDestroy {
     });
   }
 
+
+
   otpNotRecieved() {
     this.modalService.close('otpWindow');
     this.modalService.open('otpChangeNumber');
+  }
+
+  /**
+   * Is it a valid phone number
+   */
+  isPhoneValid(event: any) {
+  }
+
+
+  /**
+   * Get Phone number state
+   */
+  getNumberState(e: any) {
   }
 
   /**
@@ -414,6 +448,5 @@ export class RegistrationBasicComponent implements OnInit, OnDestroy {
   }
   ngOnDestroy() {
     document.body.style.overflow = null;
-    this.usernameCtrlSub.unsubscribe();
   }
 }
