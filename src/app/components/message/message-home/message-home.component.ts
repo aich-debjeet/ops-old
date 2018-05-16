77005b27abfa180011505d17b8e89536a8078964
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-05-16 19:26:31 +0530

    Disabled: Typing feature (performance issue)

--- src/app/components/message/message-home/message-home.component.ts
+++ src/app/components/message/message-home/message-home.component.ts
@@ -380,13 +380,13 @@ export class MessageHomeComponent implements OnInit, OnDestroy, AfterViewChecked
       && this.profileState['profile_cards']['active']
       && e.keyCode !== 13 // to prevent indication on message sent
     ) {
-      this.messageStore.dispatch({
-        type: MessageActions.USER_IS_TYPING,
-        payload: {
-          loggedInUsersHandle: this.profileState['profile_cards']['active']['handle'],
-          selectedUsersHandle: this.selectedUser.handle
-        }
-      });
+      // this.messageStore.dispatch({
+      //   type: MessageActions.USER_IS_TYPING,
+      //   payload: {
+      //     loggedInUsersHandle: this.profileState['profile_cards']['active']['handle'],
+      //     selectedUsersHandle: this.selectedUser.handle
+      //   }
+      // });
     }
     // filter emtpy mesage and spaces
     if (!this.messageText.replace(/\s/g, '').length) {
