7b2f8fe8b26462464d075bd60f8dfde1d2f20cde
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-05-23 18:10:51 +0530

    Cleanup: Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
-import { environment } from '../../../environments/environment';
 
 import { Store } from '@ngrx/store';
-import { Login, UserTag, initialTag, RightBlockTag } from '../../models/auth.model';
+import { Login, initialTag } from '../../models/auth.model';
 
-// Action
-import { AuthActions } from '../../actions/auth.action'
+// actions
+import { AuthActions } from '../../actions/auth.action';
 
 // rx
 import { Observable } from 'rxjs/Observable';
 import { Subscription, ISubscription } from 'rxjs/Subscription';
+
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -20,15 +21,13 @@ import { Subscription, ISubscription } from 'rxjs/Subscription';
 })
 
 export class LoginComponent implements OnInit, OnDestroy {
-  private tagStateSubscription: Subscription;
-  tagState$: Observable<Login>;
-  rightCom: RightBlockTag;
-  petTag = initialTag;
+  loginState$: Observable<Login>;
+  loginState = initialTag;
   loginForm: FormGroup;
-  redrectUrl: any;
+  redirectUrl: any;
   queryParam: any;
   imageBaseUrl = environment.API_IMAGE;
-  private subscription: ISubscription;
+  private loginSub: ISubscription;
 
   constructor(
     fb: FormBuilder,
@@ -37,20 +36,20 @@ export class LoginComponent implements OnInit, OnDestroy {
     public route: ActivatedRoute,
   ) {
     this.loginForm = fb.group({
-      'email' : [null, Validators.required],
-      'password': ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
     })
 
-    this.tagState$ = store.select('loginTags');
-    this.subscription = this.tagState$.subscribe((state) => {
-      this.petTag = state;
+    this.loginState$ = store.select('loginTags');
+    this.loginSub = this.loginState$.subscribe((state) => {
+      this.loginState = state;
     });
   }
 
   ngOnInit() {
     // if redriect url there
     if (this.route.snapshot.queryParams['next']) {
-      this.redrectUrl = this.route.snapshot.queryParams['next'];
+      this.redirectUrl = this.route.snapshot.queryParams['next'];
     }
 
     // redrection query param
@@ -70,19 +69,19 @@ export class LoginComponent implements OnInit, OnDestroy {
   submitForm(value: any) {
     if (this.loginForm.valid === true) {
       const form =  {
-        'client_id' : 'AKIAI7P3SOTCRBKNR3IA',
-        'client_secret': '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',
-        'username' : value.email,
-        'password' : value.password,
-        'grant_type' : 'password'
+        client_id: 'AKIAI7P3SOTCRBKNR3IA',
+        client_secret: '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',
+        username: value.email,
+        password: value.password,
+        grant_type: 'password'
       }
       this.store.dispatch({ type: AuthActions.USER_LOGIN, payload: form});
-      // Org Registration successfully
+      // org registration successful
       this.store.select('loginTags')
         .first(login => login['login_success'] === true)
-        .subscribe( datas => {
-          if (this.redrectUrl !== undefined) {
-            this.router.navigateByUrl(this.redrectUrl);
+        .subscribe(data => {
+          if (this.redirectUrl !== undefined) {
+            this.router.navigateByUrl(this.redirectUrl);
             return
           } else {
             this.router.navigateByUrl('/home');
@@ -93,7 +92,7 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.loginSub.unsubscribe();
   }
 
 }
