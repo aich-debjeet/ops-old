36603dd2014929448b736f161abfa637d065888c
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-06-01 16:33:28 +0530

    Fix: AOT

--- src/app/models/auth.model.ts
+++ src/app/models/auth.model.ts
@@ -17,6 +17,9 @@ export class Login {
  login_status?: boolean;
  login_uploading_data?: boolean;
  user_otp_failed?: boolean;
+ fp_user_response?: any;
+ fp_checking?: boolean;
+ fb_uploading_data?: boolean;
 }
 
 export class Register {
