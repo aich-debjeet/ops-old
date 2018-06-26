a8b3997096553bc8aca3d665b8b18a3bdd2a24b1
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-06-26 16:32:54 +0530

    WIP: Opportunity edit

---
+++ src/app/components/opportunity/opportunity-edit/opportunity-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opportunity-edit',
+  templateUrl: './opportunity-edit.component.html',
+  styleUrls: ['./opportunity-edit.component.scss']
+})
+export class OpportunityEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
