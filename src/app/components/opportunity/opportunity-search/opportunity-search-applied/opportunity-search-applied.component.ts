e1c97cddc1aa27996a11b3cdbffd2b4da459c4df
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-07-09 14:53:02 +0530

    Opps: Optimisation

--- src/app/components/opportunity/opportunity-search/opportunity-search-applied/opportunity-search-applied.component.ts
+++ src/app/components/opportunity/opportunity-search/opportunity-search-applied/opportunity-search-applied.component.ts
@@ -1,14 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-
-import { OpportunityModel } from './../../../../models/opportunity.model';
-import { Media } from './../../../../models/media.model';
-
-// rx
-import { Observable } from 'rxjs/Observable';
-import { Subscription, ISubscription } from 'rxjs/Subscription';
-
-import { Store } from '@ngrx/store';
-import { GeneralUtilities } from '../../../../helpers/general.utils';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 
 @Component({
   selector: 'app-opportunity-search-applied',
@@ -16,47 +6,13 @@ import { GeneralUtilities } from '../../../../helpers/general.utils';
   styleUrls: ['./opportunity-search-applied.component.scss']
 })
 export class OpportunitySearchAppliedComponent implements OnInit, OnDestroy {
-
-  opportunityState$: Observable<OpportunityModel>;
-  private oppsSub: ISubscription;
-  userState$: Observable<Media>;
-  userState: any;
-  opportunities = [];
-
-  /* pagination settings */
-  recordsPerPage = 10;
-  /* pagination settings */
-
-  constructor(
-    private store: Store<OpportunityModel>,
-    private mediaStore: Store<Media>,
-    private generalUtils: GeneralUtilities
-  ) {
-    // check for opportunity details
-    this.opportunityState$ = this.store.select('opportunityTags');
-
-    // check for user details
-    this.userState$ = this.mediaStore.select('profileTags');
-  }
+  @Input() opportunities: any;
+  constructor() { }
 
   ngOnInit() {
-
-    // observe the opportunity state
-    this.oppsSub = this.opportunityState$.subscribe((state) => {
-      if (typeof state !== 'undefined') {
-        if (
-          this.generalUtils.checkNestedKey(state, ['search_opportunities_result', 'opportunityResponse'])
-          && state['search_opportunities_result']['opportunityResponse'].length > 0
-        ) {
-          this.opportunities = state['search_opportunities_result']['opportunityResponse'];
-        }
-      }
-    });
-
   }
 
   ngOnDestroy() {
-    this.oppsSub.unsubscribe();
   }
 
 }
--- src/app/components/opportunity/opportunity-search/opportunity-search-recommended/opportunity-search-recommended.component.ts
+++ src/app/components/opportunity/opportunity-search/opportunity-search-recommended/opportunity-search-recommended.component.ts
@@ -1,14 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-
-import { OpportunityActions } from './../../../../actions/opportunity.action';
-import { OpportunityModel } from './../../../../models/opportunity.model';
-
-// rx
-import { Observable } from 'rxjs/Observable';
-import { ISubscription } from 'rxjs/Subscription';
-
-import { Store } from '@ngrx/store';
-import { GeneralUtilities } from '../../../../helpers/general.utils';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 
 @Component({
   selector: 'app-opportunity-search-recommended',
@@ -16,45 +6,13 @@ import { GeneralUtilities } from '../../../../helpers/general.utils';
   styleUrls: ['./opportunity-search-recommended.component.scss']
 })
 export class OpportunitySearchRecommendedComponent implements OnInit, OnDestroy {
-
-  opportunityState$: Observable<OpportunityModel>;
-  opportunityState: any;
-  opportunities = [];
-  private oppsSub: ISubscription;
-
-  /* scroll */
-  canScroll = true;
-  scrolling = 0;
-  scrollingLoad = 800;
-  /* scroll */
-
-  constructor(
-    private store: Store<OpportunityModel>,
-    private generalUtils: GeneralUtilities
-  ) {
-    // check for opportunity details
-    this.opportunityState$ = this.store.select('opportunityTags');
-  }
+  @Input() opportunities: any;
+  constructor() { }
 
   ngOnInit() {
-
-    // observe the opportunity state
-    this.oppsSub = this.opportunityState$.subscribe((state) => {
-      this.opportunityState = state;
-      if (typeof state !== 'undefined') {
-        if (
-          this.generalUtils.checkNestedKey(state, ['search_opportunities_result', 'opportunityResponse'])
-          && state['search_opportunities_result']['opportunityResponse'].length > 0
-        ) {
-          this.opportunities = state['search_opportunities_result']['opportunityResponse'];
-        }
-      }
-    });
-
   }
 
   ngOnDestroy() {
-    this.oppsSub.unsubscribe();
   }
 
 }
