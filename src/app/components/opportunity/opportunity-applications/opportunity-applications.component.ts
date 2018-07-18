92c93b69f98655aefe98ece47f404df1b49b2e31
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-07-18 19:28:23 +0530

    Add: Opportunity applications page

---
+++ src/app/components/opportunity/opportunity-applications/opportunity-applications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opportunity-applications',
+  templateUrl: './opportunity-applications.component.html',
+  styleUrls: ['./opportunity-applications.component.scss']
+})
+export class OpportunityApplicationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
