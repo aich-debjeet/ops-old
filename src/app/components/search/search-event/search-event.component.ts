4d3bc0ef3f9c2cffb5e10c2828d003ebed416feb
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-08-01 19:35:34 +0530

    WIP: Search moduke Event search inner page

---
+++ src/app/components/search/search-event/search-event.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { SearchActions } from '../../../actions/search.action';
+import { environment } from 'environments/environment';
+import { SearchModel } from '../../../models/search.model';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-search-event',
+  templateUrl: './search-event.component.html',
+  styleUrls: ['./search-event.component.scss']
+})
+export class SearchEventComponent implements OnInit {
+
+  searchState$: Observable<SearchModel>;
+  searchState: any;
+  baseUrl: string;
+  showPreloader = true;
+
+  events: any[];
+
+  /* scroll */
+  canScroll = true;
+  scrolling = 0;
+  scrollingLoad = 500;
+  /* scroll */
+
+  constructor(
+    private store: Store<SearchModel>
+  ) {
+
+    this.baseUrl = environment.API_IMAGE;
+    this.searchState$ = this.store.select('searchTags');
+
+  }
+
+  ngOnInit() {
+
+    // observe the store value
+    this.searchState$.subscribe((state) => {
+      this.searchState = state;
+      if (state) {
+        if (typeof state['search_event_data'] !== 'undefined' && state['search_event_data']['eventResponse']) {
+          this.events = state['search_event_data']['eventResponse'];
+        }
+        // hide preloader
+        if (typeof state['searching_event'] !== 'undefined'
+          && state['searching_event'] === false
+          && typeof state['search_event_success'] !== 'undefined'
+          && state['search_event_success'] === true) {
+          this.showPreloader = false;
+        }
+      }
+    });
+
+  }
+
+  /**
+   * While Scrolling trigger next api call
+   */
+  onScroll(e) {
+    this.scrolling = e.currentScrollPosition;
+    if (this.canScroll === true && this.scrollingLoad <= this.scrolling) {
+      this.showPreloader = true;
+      this.canScroll = false;
+      this.scrollingLoad += 400;
+      // check if it's first request
+      if (this.searchState && this.searchState['search_event_data'] && this.searchState['search_event_data']['scrollId']) {
+        this.store.dispatch({
+          type: SearchActions.SEARCH_EVENT,
+          payload: { scrollId: this.searchState['search_event_data']['scrollId'] }
+        });
+      }
+      setTimeout(() => {
+        this.canScroll = true;
+      }, 1000);
+    }
+  }
+
+}
