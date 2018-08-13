cd217affa0ce676d2f9b59b20fe7625c37602682
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-08-13 17:03:31 +0530

    Minor fix

--- src/app/pipes/new-line.pipe.ts
+++ src/app/pipes/new-line.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NewLinePipe implements PipeTransform {
 
   transform(value: string, args: string[]): any {
-    return value.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    return value.toString().replace(/(?:\r\n|\r|\n)/g, '<br />');
   }
 
 }
