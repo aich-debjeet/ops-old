a80f3bc620634c43b10250a9fa53d389b3f957da
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-08-14 15:41:40 +0530

    Invite validation helper function

--- src/app/helpers/form.validator.ts
+++ src/app/helpers/form.validator.ts
@@ -374,6 +374,23 @@ export class ProfileUpdateValidator {
     }
 }
 
+@Injectable()
+export class EmailValidator {
+    public static isValid(email: FormControl) {
+        const q = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (re.test(String(email).toLowerCase())) {
+                    resolve({ isInvalidEmail: false });
+                } else {
+                    resolve({ isInvalidEmail: true });
+                }
+            }, 100);
+        });
+        return q;
+    }
+}
+
 // Match password
 @Injectable()
 export class FormValidation {
