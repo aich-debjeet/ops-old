93831b9b9e89b87ce6a8829168e0ccab6c419899
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-08-14 16:55:47 +0530

    Added default image for Wiki profiles

--- src/app/shared/wiki-profile-card/wiki-profile-card.component.ts
+++ src/app/shared/wiki-profile-card/wiki-profile-card.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class WikiProfileCardComponent implements OnInit {
   @Input() profileDetails;
+  defaultImage = 'https://s3-us-west-2.amazonaws.com/ops.defaults/user-avatar-male.png';
 
   constructor() { }
 
