fe6b6947f82a30190208560390bc4c363b3ea7d9
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-08-30 16:02:37 +0530

    Linting

--- src/app/components/media/create-channel/create-channel.component.ts
+++ src/app/components/media/create-channel/create-channel.component.ts
@@ -1,29 +1,18 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
-import { Router, ActivatedRoute } from '@angular/router';
-
+import { Router } from '@angular/router';
 import { ModalService } from '../../../shared/modal/modal.component.service';
-import { ApiService } from '../../../helpers/api.service';
 import { GeneralUtilities } from '../../../helpers/general.utils';
 import { initialMedia, Media } from '../../../models/media.model';
-
-// Action
 import { AuthActions } from '../../../actions/auth.action';
-
-// rx
 import { Observable } from 'rxjs/Observable';
-import { Subscription, ISubscription } from 'rxjs/Subscription';
+import { ISubscription } from 'rxjs/Subscription';
 import { Store } from '@ngrx/store';
-
-// import { FileUploadService } from '../media/fakeService';
 import { ProfileModal, initialTag, UserCard } from '../../../models/profile.model';
 import { ProfileActions } from '../../../actions/profile.action';
 import { environment } from './../../../../environments/environment';
-
 import { map as _map } from 'lodash';
 import { ToastrService } from 'ngx-toastr';
-
 import * as _ from 'lodash';
 
 // Blog
@@ -61,46 +50,43 @@ export class CreateChannelComponent implements OnInit, OnDestroy {
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private route: ActivatedRoute,
     private toastr: ToastrService,
     private tokenService: TokenService,
-    private localStorageService: LocalStorageService,
     private generalHelper: GeneralUtilities,
-    private store: Store<Media> ) {
-      this.createChannelForm();
-      this.typeSelected = false;
-      this.stepNumber = 1;
-      this.channelSaved = false;
-      this.channelSavedHere = false;
+    private store: Store<Media>
+  ) {
+    this.createChannelForm();
+    this.typeSelected = false;
+    this.stepNumber = 1;
+    this.channelSaved = false;
+    this.channelSavedHere = false;
 
-      this.handle = '';
-      if (this.handle !== '') {
-        this.handle = this.tokenService.getHandle();
-      }
-
-      this.loginTagState$ = store.select('loginTags');
-      this.loginSub = this.loginTagState$.subscribe((state) => {
-        if (typeof state !== 'undefined') {
-          this.industries = state.industries;
-        }
-      });
-
-      this.tagState$ = this.store.select('profileTags');
-      this.profileSub = this.tagState$.subscribe((state) => {
-        if (typeof state !== 'undefined') {
-          this.profileChannel = state;
-          this.channelSaved = this.profileChannel.channel_saved;
-
-          const activeUser = this.profileChannel.profile_cards.active;
-          this.activeUser = activeUser;
-
-          if (this.channelSavedHere && this.channelSaved === true ) {
-            this.createChannelForm();
-            this.channelSavedHere = false;
-          }
-        }
-      });
+    this.handle = '';
+    if (this.handle !== '') {
+      this.handle = this.tokenService.getHandle();
     }
+
+    this.loginTagState$ = store.select('loginTags');
+    this.loginSub = this.loginTagState$.subscribe((state) => {
+      if (typeof state !== 'undefined') {
+        this.industries = state.industries;
+      }
+    });
+
+    this.tagState$ = this.store.select('profileTags');
+    this.profileSub = this.tagState$.subscribe((state) => {
+      if (typeof state !== 'undefined') {
+        this.profileChannel = state;
+        this.channelSaved = this.profileChannel.channel_saved;
+        const activeUser = this.profileChannel.profile_cards.active;
+        this.activeUser = activeUser;
+        if (this.channelSavedHere && this.channelSaved === true) {
+          this.createChannelForm();
+          this.channelSavedHere = false;
+        }
+      }
+    });
+  }
 
   ngOnInit() {
     // Loading industry list
@@ -118,10 +104,10 @@ export class CreateChannelComponent implements OnInit, OnDestroy {
   createChannelForm() {
     // Empty initiate form
     this.channelForm = this.fb.group({
-      title: ['', Validators.required ],
-      type: ['', Validators.required ],
-      desc: ['', Validators.required ],
-      privacy: [0, Validators.required ]
+      title: ['', Validators.required],
+      type: ['', Validators.required],
+      desc: ['', Validators.required],
+      privacy: [0, Validators.required]
     })
   }
 
@@ -136,35 +122,31 @@ export class CreateChannelComponent implements OnInit, OnDestroy {
     // set profile handle to user handle
     const profileHandle = this.activeUser.handle;
 
-    if ( this.channelForm.valid === true && profileHandle !== '' ) {
-
+    if (this.channelForm.valid === true && profileHandle !== '') {
       if (!this.hashTags) {
         this.hashTags = [];
       }
-
       const channelObj = {
         name: value.title,
         owner: profileHandle,
         mediaTypes: mediaTypeList,
-        industryList: [ value.type ],
+        industryList: [value.type],
         superType: 'channel',
         access: Number(value.privacy),
         description: value.desc,
-        accessSettings : { access : Number(value.privacy) },
+        accessSettings: { access: Number(value.privacy) },
         hashTags: this.hashTags
       }
-
       this.channelSavedHere = true;
       this.store.dispatch({ type: ProfileActions.CHANNEL_SAVE, payload: channelObj });
 
       this.store.select('profileTags')
-      .first(channel => channel['channel_create_success'] === true)
-      .subscribe( datas => {
+        .first(channel => channel['channel_create_success'] === true)
+        .subscribe(datas => {
           this.toastr.success('successfully created channel', 'Success!');
           this.router.navigateByUrl('/channel/' + datas['channel_created_details'].id);
           return
-      });
-
+        });
     } else {
       this.toastr.warning('Please fill all required fields');
     }
