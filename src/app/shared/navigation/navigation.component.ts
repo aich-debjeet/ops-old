a12121919479648fb35712a6a00627cbe5253c71
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-10-11 18:25:39 +0530

    Trigger scroll chat window from navigation component

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -14,6 +14,7 @@ import { GeneralUtilities } from '../../helpers/general.utils';
 import { PusherService } from '../../services/pusher.service';
 import { MessageActions } from '../../actions/message.action';
 import { MessageModal } from '../../models/message.model';
+import { MessageHomeComponent } from 'app/components/message/message-home/message-home.component';
 
 @Component({
   selector: 'app-navigation',
@@ -66,6 +67,7 @@ export class NavigationComponent implements OnInit, OnDestroy {
     public generalHelper: GeneralUtilities,
     private router: Router,
     private pusherService: PusherService,
+    private msgHome: MessageHomeComponent,
   ) {
 
     this.topNav = {
@@ -246,9 +248,9 @@ export class NavigationComponent implements OnInit, OnDestroy {
               payload: message
             });
           }
-          // setTimeout(() => {
-          //   this.scrollToBottom();
-          // }, 20);
+          setTimeout(() => {
+            this.msgHome.scrollToBottom();
+          }, 20);
         });
       }
 
