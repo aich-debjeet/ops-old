934db26e67a09616a8ced7175147d8b6e8237463
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-11-19 17:21:28 +0530

    WIP: Request for an Invite

--- src/app/components/logout-home/logout-home.component.ts
+++ src/app/components/logout-home/logout-home.component.ts
@@ -1,20 +1,75 @@
-import { Component, OnInit, OnDestroy, Inject  } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormValidation } from 'app/helpers/form.validator';
+import { Store } from '@ngrx/store';
+import { AuthActions } from 'app/actions/auth.action';
+import { Modal } from 'app/shared/modal-new/Modal';
+import { ModalService } from 'app/shared/modal/modal.component.service';
 @Component({
   selector: 'app-logout-home',
   templateUrl: './logout-home.component.html',
   styleUrls: ['./logout-home.component.scss']
 })
-export class LogoutHomeComponent implements OnInit, OnDestroy {
+export class LogoutHomeComponent implements OnInit {
 
   base_image = environment.API_IMAGE;
+  inviteForm: FormGroup;
+  disableSubmit = false;
+  @ViewChild('inviteSuccessModal') inviteSuccessModal: Modal;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<any>,
+    public modalService: ModalService
+  ) { }
 
   ngOnInit() {
+    // this.inviteSuccessModal.open();
+    this.buildForm();
   }
 
-  ngOnDestroy() {
+  /**
+   * bulding reactive form getting an email id
+   */
+  buildForm() {
+    this.inviteForm = this.fb.group({
+      emailId: ['', [
+        Validators.required,
+        Validators.min(3),
+        FormValidation.validEmail
+      ]]
+    });
+  }
+
+  /**
+   * submitting the form
+   */
+  sendInvite() {
+    if (!this.inviteForm.valid) {
+      return false;
+    }
+    this.disableSubmit = true;
+    const reqBody = {
+      listData: [{ email: this.inviteForm.controls.emailId.value }]
+    };
+    this.store.dispatch({
+      type: AuthActions.SEND_INVITATION,
+      payload: reqBody
+    });
+
+    this.store.select('loginTags')
+      .first(resp => resp['send_invite_success'] === true)
+      .subscribe(() => {
+        this.disableSubmit = false;
+        this.inviteSuccessModal.open();
+        this.buildForm();
+        return;
+      });
+  }
+
+  openModal() {
+    this.inviteSuccessModal.open();
   }
 
 }
