07d744bcfa1d9446766036267ce70fe7d8ef24dc
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-11-23 12:48:06 +0530

    Done: Bookamark images tab (basic)

--- src/app/components/bookmark/bookmark-image/bookmark-image.component.ts
+++ src/app/components/bookmark/bookmark-image/bookmark-image.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BookmarkModel } from 'app/models/bookmark.model';
+import { Store } from '@ngrx/store';
+import { ISubscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { GeneralUtilities } from 'app/helpers/general.utils';
 
 @Component({
   selector: 'app-bookmark-image',
   templateUrl: './bookmark-image.component.html',
   styleUrls: ['./../bookmark.component.scss']
 })
-export class BookmarkImageComponent implements OnInit {
+export class BookmarkImageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  showPreloader = true;
+  bookmarkSub: ISubscription;
+  bookmarkStore$: Observable<BookmarkModel>;
+  bookmarkState: any;
+  bookmarks = [];
+
+  constructor(
+    private store: Store<BookmarkModel>,
+    private gUtils: GeneralUtilities
+  ) {
+    this.bookmarkStore$ = this.store.select('bookmarkStore');
+    this.bookmarkSub = this.bookmarkStore$.subscribe((state) => {
+      this.bookmarkState = state;
+      if (this.gUtils.checkNestedKey(state, ['bookmarkType']) && state['bookmarkType'] === 'image') {
+        if (state.loadingBookmarks === false && state.loadedBookmarks === true) {
+          this.showPreloader = false;
+          this.bookmarks = state.bookmarkData['bookmarkedPosts'];
+        }
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.bookmarkSub.unsubscribe();
   }
 
 }
