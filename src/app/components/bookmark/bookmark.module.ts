21d49a79d5d46693f816dbb40b3634b5a6724039
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-11-23 19:19:30 +0530

    Add: Shared pipes module for UTC date filter

--- src/app/components/bookmark/bookmark.module.ts
+++ src/app/components/bookmark/bookmark.module.ts
@@ -18,6 +18,7 @@ import { BookmarkVideoCardComponent } from './cards/bookmark-video-card/bookmark
 import { BookmarkEventCardComponent } from './cards/bookmark-event-card/bookmark-event-card.component';
 import { BookmarkOpportunityCardComponent } from './cards/bookmark-opportunity-card/bookmark-opportunity-card.component';
 import { BookmarkProfileCardComponent } from './cards/bookmark-profile-card/bookmark-profile-card.component';
+import { SharedPipesModule } from 'app/pipes/shared-pipes.module';
 
 const routes: Routes = [
   {
@@ -41,7 +42,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    SharedModule
+    SharedModule,
+    SharedPipesModule
   ],
   declarations: [
     BookmarkComponent,
