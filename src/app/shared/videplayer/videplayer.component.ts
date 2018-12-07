f2f53b1dd36795f5f5d5f019be0add96afed5b3e
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2018-12-07 15:36:03 +0530

    WIP: Video player autoplay issue

--- src/app/shared/videplayer/videplayer.component.ts
+++ src/app/shared/videplayer/videplayer.component.ts
@@ -1,62 +1,60 @@
-import { Component, OnInit, Input, AfterViewInit, Renderer } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { GeneralUtilities } from 'app/helpers/general.utils';
+import { VgAPI } from 'videogular2/core';
+import { IPlayable } from 'videogular2/src/core/vg-media/i-playable';
 
 @Component({
   selector: 'app-videoplayer',
   templateUrl: './videplayer.component.html',
   styleUrls: ['./videplayer.component.scss']
 })
-export class VideplayerComponent implements OnInit, AfterViewInit {
+export class VideplayerComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() src: any;
   @Input() showControls = false;
   preload = 'auto';
   source: any;
   isPortrait = false; // assuming video is landscape by default
-  constructor(
-    // private renderer: Renderer
-  ) {
+  gUtilsSub: Subscription;
+  videoPlayer: IPlayable;
+
+  constructor(private gUtils: GeneralUtilities) {
     this.source = this.src;
-    // console.log('trigger again constructor');
   }
 
   ngOnInit() {
+    this.gUtilsSub = this.gUtils.listen().subscribe((e: any) => {
+      if (e.component && e.component === 'VideplayerComponent') {
+        if (this.videoPlayer && e.action === 'playVideo') {
+          this.videoPlayer.play();
+        }
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.gUtilsSub.unsubscribe();
+  }
+
+  onPlayerReady(api: VgAPI) {
+    this.videoPlayer = api.getDefaultMedia();
   }
 
   ngAfterViewInit() {
+    // const videoElem: HTMLVideoElement = <HTMLVideoElement>document.getElementById('videoElem');
+    // // console.log('videoElem', videoElem);
     // const self = this;
-    // console.log('trigger again view init');
-
-    // const videoElem: HTMLVideoElement = <HTMLVideoElement>document.getElementById('singleVideo');
-    // this.renderer.listen(videoElem, 'loadeddata', (event) => {
-    //   // console.log('video event', event);
-    //   // console.log('videoElem loaded');
-    //     const vidH = videoElem.videoHeight;
-    //     const vidW = videoElem.videoWidth;
-    //     console.log(vidH, vidW);
-    //     // check if video is portrait
-    //     if (vidH > vidW) {
-    //       self.isPortrait = true;
-    //       // console.log('video portrait');
-    //     } else {
-    //       // console.log('video lanscape');
-    //     }
+    // videoElem.addEventListener('loadeddata', function() {
+    //   const vidH = videoElem.videoHeight;
+    //   const vidW = videoElem.videoWidth;
+    //   // check if video is portrait
+    //   if (vidH > vidW) {
+    //     self.isPortrait = true;
+    //     console.log('video portrait');
+    //   } else {
+    //     console.log('video lanscape');
+    //   }
     // });
-    const videoElem: HTMLVideoElement = <HTMLVideoElement>document.getElementById('singleVideo');
-    // console.log('videoElem', videoElem);
-    const self = this;
-    videoElem.addEventListener('loadeddata', function() {
-      // console.log('videoElem loaded');
-      const vidH = videoElem.videoHeight;
-      const vidW = videoElem.videoWidth;
-      // console.log(vidH, vidW);
-
-      // check if video is portrait
-      if (vidH > vidW) {
-        self.isPortrait = true;
-        // console.log('video portrait');
-      } else {
-        // console.log('video lanscape');
-      }
-    });
   }
 
 }
