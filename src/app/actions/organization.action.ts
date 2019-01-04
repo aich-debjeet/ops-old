e0ec393374231305a7756da6d94916e3c683e8d5
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2019-01-04 15:24:28 +0530

    Organisation actions celanup, linting

--- src/app/actions/organization.action.ts
+++ src/app/actions/organization.action.ts
@@ -1,123 +1,62 @@
 export class OrganizationActions {
 
-    // Organization Registration
-    static ORGANIZATION_REGISTRATION = 'ORGANIZATION_REGISTRATION';
-    static ORGANIZATION_REGISTRATION_SUCCESS = 'ORGANIZATION_REGISTRATION_SUCCESS';
-    static ORGANIZATION_REGISTRATION_FAILED = 'ORGANIZATION_REGISTRATION_FAILED';
+  // Organization Registration
+  static ORGANIZATION_REGISTRATION = 'ORGANIZATION_REGISTRATION';
+  static ORGANIZATION_REGISTRATION_SUCCESS = 'ORGANIZATION_REGISTRATION_SUCCESS';
+  static ORGANIZATION_REGISTRATION_FAILED = 'ORGANIZATION_REGISTRATION_FAILED';
 
-    // Load Organization
-    static LOAD_ORGANIZATION = 'LOAD_ORGANIZATION';
-    static LOAD_ORGANIZATION_SUCCESS = 'LOAD_ORGANIZATION_SUCCESS';
-    static LOAD_ORGANIZATION_FAILED = 'LOAD_ORGANIZATION_FAILED';
+  // Load Organization
+  static LOAD_ORGANIZATION = 'LOAD_ORGANIZATION';
+  static LOAD_ORGANIZATION_SUCCESS = 'LOAD_ORGANIZATION_SUCCESS';
+  static LOAD_ORGANIZATION_FAILED = 'LOAD_ORGANIZATION_FAILED';
 
-    // Organization image upload Server
-    static IMAGE_UPLOAD_SERVER = 'IMAGE_UPLOAD_SERVER';
-    static IMAGE_UPLOAD_SERVER_SUCCESS = 'IMAGE_UPLOAD_SERVER_SUCCESS';
-    static IMAGE_UPLOAD_SERVER_FAILED = 'IMAGE_UPLOAD_SERVER_FAILED';
+  // Organization image upload Server
+  static IMAGE_UPLOAD_SERVER = 'IMAGE_UPLOAD_SERVER';
+  static IMAGE_UPLOAD_SERVER_SUCCESS = 'IMAGE_UPLOAD_SERVER_SUCCESS';
+  static IMAGE_UPLOAD_SERVER_FAILED = 'IMAGE_UPLOAD_SERVER_FAILED';
 
-    // Organization profile image upload
-    static ORG_PROFILE_IMAGE_UPLOAD = 'ORG_PROFILE_IMAGE_UPLOAD';
-    static ORG_PROFILE_IMAGE_UPLOAD_SUCCESS = 'ORG_PROFILE_IMAGE_UPLOAD_SUCCESS';
-    static ORG_PROFILE_IMAGE_UPLOAD_FAILED = 'ORG_PROFILE_IMAGE_UPLOAD_FAILED';
+  // Organization profile image upload
+  static ORG_PROFILE_IMAGE_UPLOAD = 'ORG_PROFILE_IMAGE_UPLOAD';
+  static ORG_PROFILE_IMAGE_UPLOAD_SUCCESS = 'ORG_PROFILE_IMAGE_UPLOAD_SUCCESS';
+  static ORG_PROFILE_IMAGE_UPLOAD_FAILED = 'ORG_PROFILE_IMAGE_UPLOAD_FAILED';
 
-    // Organization profile Update
-    static ORG_PROFILE_UPDATE = 'ORG_PROFILE_UPDATE';
-    static ORG_PROFILE_UPDATE_SUCCESS = 'ORG_PROFILE_UPDATE_SUCCESS';
-    static ORG_PROFILE_UPDATE_FAILED = 'ORG_PROFILE_UPDATE_FAILED';
+  // Organization profile Update
+  static ORG_PROFILE_UPDATE = 'ORG_PROFILE_UPDATE';
+  static ORG_PROFILE_UPDATE_SUCCESS = 'ORG_PROFILE_UPDATE_SUCCESS';
+  static ORG_PROFILE_UPDATE_FAILED = 'ORG_PROFILE_UPDATE_FAILED';
 
-    // Get Organization profile Details
-    static ORG_PROFILE_DETAILS = 'ORG_PROFILE_DETAILS';
-    static ORG_PROFILE_DETAILS_SUCCESS = 'ORG_PROFILE_DETAILS_SUCCESS';
-    static ORG_PROFILE_DETAILS_FAILED = 'ORG_PROFILE_DETAILS_FAILED';
-    static ORGANIZATION_DELETE = 'ORGANIZATION_DELETE';
-    static ORGANIZATION_DELETE_SUCCESS = 'ORGANIZATION_DELETE_SUCCESS';
-    static ORGANIZATION_DELETE_FAILED = 'ORGANIZATION_DELETE_FAILED';
+  // Get Organization profile Details
+  static ORG_PROFILE_DETAILS = 'ORG_PROFILE_DETAILS';
+  static ORG_PROFILE_DETAILS_SUCCESS = 'ORG_PROFILE_DETAILS_SUCCESS';
+  static ORG_PROFILE_DETAILS_FAILED = 'ORG_PROFILE_DETAILS_FAILED';
+  static ORGANIZATION_DELETE = 'ORGANIZATION_DELETE';
+  static ORGANIZATION_DELETE_SUCCESS = 'ORGANIZATION_DELETE_SUCCESS';
+  static ORGANIZATION_DELETE_FAILED = 'ORGANIZATION_DELETE_FAILED';
 
-    static GET_RECEIPIENT = 'GET_RECEIPIENT';
-    static GET_RECEIPIENT_SUCCESS = 'GET_RECEIPIENT_SUCCESS';
-    static GET_RECEIPIENT_FAILED = 'GET_RECEIPIENT_FAILED';
+  static GET_RECEIPIENT = 'GET_RECEIPIENT';
+  static GET_RECEIPIENT_SUCCESS = 'GET_RECEIPIENT_SUCCESS';
+  static GET_RECEIPIENT_FAILED = 'GET_RECEIPIENT_FAILED';
 
-    static GET_MEMBERS = 'GET_MEMBERS';
-    static GET_MEMBERS_SUCCESS = 'GET_MEMBERS_SUCCESS';
-    static GET_MEMBERS_FAILED = 'GET_MEMBERS_FAILED';
+  static GET_MEMBERS = 'GET_MEMBERS';
+  static GET_MEMBERS_SUCCESS = 'GET_MEMBERS_SUCCESS';
+  static GET_MEMBERS_FAILED = 'GET_MEMBERS_FAILED';
 
-    static GET_ORGANIZATION_BY_HANDLE = 'GET_ORGANIZATION_BY_HANDLE';
-    static GET_ORGANIZATION_BY_HANDLE_SUCCESS = 'GET_ORGANIZATION_BY_HANDLE_SUCCESS';
-    static GET_ORGANIZATION_BY_HANDLE_FAILED = 'GET_ORGANIZATION_BY_HANDLE_FAILED';
+  static GET_ORGANIZATION_BY_HANDLE = 'GET_ORGANIZATION_BY_HANDLE';
+  static GET_ORGANIZATION_BY_HANDLE_SUCCESS = 'GET_ORGANIZATION_BY_HANDLE_SUCCESS';
+  static GET_ORGANIZATION_BY_HANDLE_FAILED = 'GET_ORGANIZATION_BY_HANDLE_FAILED';
 
-    static LOAD_ORG_CHANNELS = 'LOAD_ORG_CHANNELS';
-    static LOAD_ORG_CHANNELS_SUCCESS = 'LOAD_ORG_CHANNELS_SUCCESS';
-    static LOAD_ORG_CHANNELS_FAILED = 'LOAD_ORG_CHANNELS_FAILED';
+  static LOAD_ORG_CHANNELS = 'LOAD_ORG_CHANNELS';
+  static LOAD_ORG_CHANNELS_SUCCESS = 'LOAD_ORG_CHANNELS_SUCCESS';
+  static LOAD_ORG_CHANNELS_FAILED = 'LOAD_ORG_CHANNELS_FAILED';
 
-    // Get Organization profile navigation details
-    static ORG_PROFILE = 'ORG_PROFILE';
-    static ORG_PROFILE_SUCCESS = 'ORG_PROFILE_SUCCESS';
-    static ORG_PROFILE_FAILED = 'ORG_PROFILE_FAILED';
+  // Get Organization profile navigation details
+  static ORG_PROFILE = 'ORG_PROFILE';
+  static ORG_PROFILE_SUCCESS = 'ORG_PROFILE_SUCCESS';
+  static ORG_PROFILE_FAILED = 'ORG_PROFILE_FAILED';
 
-    // invite people to join organisation
-    static INVITE_MEMBER = 'INVITE_MEMBER';
-    static INVITE_MEMBER_SUCCESS = 'INVITE_MEMBER_SUCCESS';
-    static INVITE_MEMBER_FAILED = 'INVITE_MEMBER_FAILED';
+  // invite people to join organisation
+  static INVITE_MEMBER = 'INVITE_MEMBER';
+  static INVITE_MEMBER_SUCCESS = 'INVITE_MEMBER_SUCCESS';
+  static INVITE_MEMBER_FAILED = 'INVITE_MEMBER_FAILED';
 
-    /*------------------------ get receipient details---------------------------------*/
-
-  getReceipient(value) {
-    return {
-      type: OrganizationActions.GET_RECEIPIENT,
-      payload: { value }
-    };
-  }
-  getReceipientSuccess (value) {
-    return {
-      type: OrganizationActions.GET_RECEIPIENT_SUCCESS,
-      payload: { value }
-    };
-  }
-  getReceipientFailure (value) {
-    return {
-      type: OrganizationActions.GET_RECEIPIENT_FAILED,
-      payload: { value }
-    };
-  }
-
-  /*------------------------ get organization members---------------------------------*/
-  getMembers(value) {
-    return {
-      type: OrganizationActions.GET_MEMBERS,
-      payload: { value }
-    };
-  }
-  getMembersSuccess (value) {
-    return {
-      type: OrganizationActions.GET_MEMBERS_SUCCESS,
-      payload: { value }
-    };
-  }
-  getMembersFailure (value) {
-    return {
-      type: OrganizationActions.GET_MEMBERS_FAILED,
-      payload: { value }
-    };
-  }
-
-
-  /*------------------------ get organization settings details by handle---------------------------------*/
-  getDefaultSettings(value) {
-    return {
-      type: OrganizationActions.GET_ORGANIZATION_BY_HANDLE,
-      payload: { value }
-    };
-  }
-  getDefaultSettingsSuccess (value) {
-    return {
-      type: OrganizationActions.GET_ORGANIZATION_BY_HANDLE_SUCCESS,
-      payload: { value }
-    };
-  }
-  getDefaultSettingsFailure (value) {
-    return {
-      type: OrganizationActions.GET_ORGANIZATION_BY_HANDLE_FAILED,
-      payload: { value }
-    };
-  }
 }
