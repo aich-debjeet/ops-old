b2a8adbe9a516c604fb6108be5b2c3bc3f1ed156
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2019-01-17 17:13:32 +0530

    Fix: OPS-2420

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -203,7 +203,7 @@ export class PortfolioComponent implements OnInit, OnDestroy {
    * add category
    */
   portAddCategory(formData: any) {
-    if (this.portAddCategoryForm.controls.categoryName.value === '') {
+    if (this.portAddCategoryForm.controls.categoryName.value.trim() === '') {
       this.catNameIsRequired = true;
       return;
     } else {
@@ -215,7 +215,7 @@ export class PortfolioComponent implements OnInit, OnDestroy {
       if (this.portAddCategoryForm.valid === true) {
         this.profileStore.dispatch({
           type: ProfileActions.ADD_PORTFOLIO_CATEGORY,
-          payload: { name: formData.categoryName }
+          payload: { name: formData.categoryName.trim() }
         });
         this.portAddCategoryForm.controls.categoryName.setValue('');
       }
