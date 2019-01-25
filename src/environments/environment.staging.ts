a005538e1f359e2d855011007451c1bd5ff64ff0
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2019-01-25 12:10:12 +0530

    Update: Environment vars

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,9 +1,10 @@
 export const environment = {
+  type: 'staging',
+  production: false,
   pusher: {
     cluster: 'ap2',
     key: 'c128d96cef8f947fc721',
   },
-  production: true,
   API_ENDPOINT: 'https://stg.api.onepagespotlight.com/api/1.0',
   API_IMAGE: 'https://cdn.onepagespotlight.com/',
   API_DOMAIN: 'https://stg.onepagespotlight.com/'
