e7e4a2f4ec7c1572acf04e7ea1cfc0fdd783d4e5
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2019-02-07 12:33:51 +0530

    feat(improve): signup DOB date and age validation

--- src/app/helpers/form.validator.ts
+++ src/app/helpers/form.validator.ts
@@ -537,44 +537,71 @@ export class FormValidation {
         return null;
     }
 
+    // for date mask
     static validateAgeSignup(control: AbstractControl) {
-        const dob = control.value.formatted;
-        if (dob) {
-            const dateArr =  dob.split('-');
-            const day = dateArr[0];
-            const month = dateArr[1];
-            const year = dateArr[2];
-
-            const a = moment();
-            const b = moment([year, month, day]);
-
-            const years = a.diff(b, 'year');
-            b.add(years, 'years');
-
-            const months = a.diff(b, 'months');
-            b.add(months, 'months');
-
-            const days = a.diff(b, 'days');
-            // console.log(years + ' years ' + months + ' months ' + days + ' days');
-
-            if (years < 12) {
+        const dob = control.value;
+        if (dob === '' || dob.includes('_')) { // check for the full date
+            // console.log('not completed');
+            return null;
+        }
+        if (moment(dob, 'MM-DD-YYYY', true).isValid()) { // check for valid date
+            // console.log('valid date');
+            const age = moment().diff(dob, 'years');
+            // console.log('age', age);
+            if (age <= 12) {
                 // console.log('isUnderAge');
                 return { isUnderAge: true };
-            } else if (years === 12) {
-                if (months < 11) {
-                    // console.log('isUnderAge');
-                    return { isUnderAge: true };
-                } else if (months === 11) {
-                    if (days >= 0) { }
-                }
             }
-
-            if (years >= 100) {
+            if (age >= 100) {
+                // console.log('isOverAge');
                 return { isOverAge: true };
             }
+        } else {
+            // console.log('invalid date');
+            return { invalidDOB: true };
         }
         return null;
     }
+
+    // for date picker
+    // static validateAgeSignup(control: AbstractControl) {
+    //     const dob = control.value.formatted;
+    //     if (dob) {
+    //         const dateArr =  dob.split('-');
+    //         const day = dateArr[0];
+    //         const month = dateArr[1];
+    //         const year = dateArr[2];
+
+    //         const a = moment();
+    //         const b = moment([year, month, day]);
+
+    //         const years = a.diff(b, 'year');
+    //         b.add(years, 'years');
+
+    //         const months = a.diff(b, 'months');
+    //         b.add(months, 'months');
+
+    //         const days = a.diff(b, 'days');
+    //         // console.log(years + ' years ' + months + ' months ' + days + ' days');
+
+    //         if (years < 12) {
+    //             // console.log('isUnderAge');
+    //             return { isUnderAge: true };
+    //         } else if (years === 12) {
+    //             if (months < 11) {
+    //                 // console.log('isUnderAge');
+    //                 return { isUnderAge: true };
+    //             } else if (months === 11) {
+    //                 if (days >= 0) { }
+    //             }
+    //         }
+
+    //         if (years >= 100) {
+    //             return { isOverAge: true };
+    //         }
+    //     }
+    //     return null;
+    // }
 
     static matchPassword(AC: AbstractControl) {
         const password = AC.get('password').value; // to get value in input tag
