34276fdf85208178f804e59fe7e5cd8017aba211
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2019-02-08 14:36:19 +0530

    add: meta and OG tags for social media, SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Router, NavigationEnd, RouteConfigLoadStart, RouteConfigLoadEnd } from 
 import { NgProgress } from 'ngx-progressbar';
 import { ISubscription } from 'rxjs/Subscription';
 import { environment } from 'environments/environment';
+import { Meta } from '@angular/platform-browser';
 
 declare let ga;
 @Component({
@@ -18,8 +19,52 @@ export class AppComponent implements OnInit, OnDestroy {
   constructor(
     public ngProgress: NgProgress,
     public renderer: Renderer,
-    public router: Router
-  ) { }
+    public router: Router,
+    private meta: Meta,
+  ) {
+    // search engine
+    this.meta.addTags([
+      { name: 'title', content: 'One Page Spotlight' },
+      { name: 'image', content: 'https://cdn.onepagespotlight.com/v2/index/logo.svg' },
+      { name: 'description', content: 'An online hub for artists and creators. Create your profile, highlight your work, build connections and network exclusively with artists, showcase your portfolio, and find the best opportunities.' },
+      { name: 'keywords', content: '' }
+    ]);
+
+    // schema.org
+    this.meta.addTags([
+      { itemprop: 'name', content: 'One Page Spotlight' },
+      { itemprop: 'image', content: 'https://cdn.onepagespotlight.com/v2/index/logo.svg' },
+      { itemprop: 'description', content: 'An online hub for artists and creators. Create your profile, highlight your work, build connections and network exclusively with artists, showcase your portfolio, and find the best opportunities.' },
+    ]);
+
+    // twitter
+    this.meta.addTags([
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:title', content: 'One Page Spotlight' },
+      { name: 'twitter:description', content: 'An online hub for artists and creators. Create your profile, highlight your work, build connections and network exclusively with artists, showcase your portfolio, and find the best opportunities.' },
+      { name: 'twitter:site', content: '@1pagespotlight' },
+      { name: 'twitter:image:src', content: 'https://cdn.onepagespotlight.com/v2/ops_icon.png' },
+    ]);
+
+    // open graph (facebook, pinterest, google+ etc.)
+    this.meta.addTags([
+      { name: 'og:title', content: 'One Page Spotlight' },
+      { name: 'og:description', content: 'An online hub for artists and creators. Create your profile, highlight your work, build connections and network exclusively with artists, showcase your portfolio, and find the best opportunities.' },
+      { name: 'og:image', content: 'https://cdn.onepagespotlight.com/v2/ops_icon.png' },
+      { name: 'og:url', content: 'https://onepagespotlight.com' },
+      { name: 'og:site_name', content: 'One Page Spotlight' },
+      { name: 'og:type', content: 'website' },
+    ]);
+
+    this.meta.addTags([
+      { property: 'og:title', content: 'One Page Spotlight' },
+      { property: 'og:description', content: 'An online hub for artists and creators. Create your profile, highlight your work, build connections and network exclusively with artists, showcase your portfolio, and find the best opportunities.' },
+      { property: 'og:image', content: 'https://cdn.onepagespotlight.com/v2/ops_icon.png' },
+      { property: 'og:url', content: 'https://onepagespotlight.com' },
+      { property: 'og:site_name', content: 'One Page Spotlight' },
+      { property: 'og:type', content: 'website' },
+    ]);
+  }
 
   ngOnInit() {
 
